75c7d20098eca4ac1cad386b40624265916af593
Egil Hansen <egil@assimilated.dk>
2019-07-31 08:26:49 +0000

    added sample project

---
+++ sample/ComponentLibTests/Component1Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Egil.RazorComponents.Testing;
+using Xunit;
+
+namespace ComponentLib
+{
+    public class Component1Test : RazorComponentFixture
+    {
+        [Fact]
+        public void Component1Test_1()
+        {
+            var expectedHtml = @"<div class=""my-component"">
+                                     This Blazor component is defined in the <strong>ComponentLib</strong> package.
+                                 </div>
+                                 ";
+
+            var result = Component<Component1>().Render();
+
+            result.ShouldBe(expectedHtml);
+        }
+
+        [Fact]
+        public void Component1Test_1_FAILS()
+        {
+            var expectedHtml = @"<hr />";
+
+            var result = Component<Component1>().Render();
+
+            result.ShouldBe(expectedHtml);
+        }
+    }
+}
