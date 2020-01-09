289c74ee8dd2da697181cd5458ae7bba80079628
Egil Hansen <egil@assimilated.dk>
2020-01-09 10:59:36 +0000

    Basic example tests

---
+++ sample/src/Data/Localizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Egil.RazorComponents.Testing.SampleApp.Data
+{
+    public class Localizer
+    {
+        private readonly Dictionary<string, string> _localizations
+            = new Dictionary<string, string>();
+
+        public string CultureCode { get; set; } = "en";
+
+        public string this[string key]
+        {
+            get => _localizations.TryGetValue(key, out var localized)
+                ? localized
+                : key;
+        }
+
+        public void Add(string key, string text)
+        {
+            _localizations.Add(key, text);
+        }
+
+        public static Localizer Empty { get; } = new Localizer();
+    }
+}
