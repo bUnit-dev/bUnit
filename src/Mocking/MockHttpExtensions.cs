ef02c397e42ed022a05034d04efa5271f8f4a117
Egil Hansen <egil@assimilated.dk>
2020-01-11 13:17:34 +0000

    Added docs to MockHttp extensions

--- src/Mocking/MockHttpExtensions.cs
+++ src/Mocking/MockHttpExtensions.cs
@@ -9,8 +9,17 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Egil.RazorComponents.Testing
 {
+    /// <summary>
+    /// Helper methods for adding a Mock HTTP client to a service provider.
+    /// </summary>
     public static class MockHttpExtensions
     {
+        /// <summary>
+        /// Create a <see cref="MockHttpMessageHandler"/> and adds it to the 
+        /// <paramref name="serviceProvider"/> as a <see cref="HttpClient"/>.
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns>The <see cref="MockHttpMessageHandler"/>.</returns>
         public static MockHttpMessageHandler AddMockHttp(this TestServiceProvider serviceProvider)
         {
             if (serviceProvider is null) throw new ArgumentNullException(nameof(serviceProvider));
@@ -22,6 +31,13 @@ namespace Egil.RazorComponents.Testing
             return mockHttp;
         }
 
+        /// <summary>
+        /// Configure a <see cref="MockHttpMessageHandler"/> to capture requests to 
+        /// a <paramref name="url"/>.
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <param name="url">Url of requests to capture.</param>
+        /// <returns>A <see cref="TaskCompletionSource{TResult}"/> that can be used to send a response to a captured request.</returns>
         [SuppressMessage("Reliability", "CA2008:Do not create tasks without passing a TaskScheduler", Justification = "<Pending>")]
         [SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "<Pending>")]
         public static TaskCompletionSource<object> Capture(this MockHttpMessageHandler handler, string url)
