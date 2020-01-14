057e005e67022fcc10a5789ecab7f9c644fb79f7
Egil Hansen <egil@assimilated.dk>
2020-01-14 11:40:31 +0000

    Dotnetconf samples (#33)
    * Basic example tests
    
    * Add missing MarkupMatches overload
    
    * Added docs to MockHttp extensions
    
    * dotnet conf samples

---
+++ sample/src/Components/DismissingEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egil.RazorComponents.Testing.SampleApp.Components
+{
+    public class DismissingEventArgs
+    {
+        public Alert Sender { get; }
+
+        public bool Cancel { get; set; }
+
+        public DismissingEventArgs(Alert sender)
+        {
+            Sender = sender;
+        }
+    }
+}
