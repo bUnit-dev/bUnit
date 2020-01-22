7411c68743848f31b7f7965f5cc25a111c5423d7
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2020-01-22 02:24:35 -0500

    Add default JsRuntime (#32)
    * Add default JsRuntime
    
    * Update src/Mocking/JSInterop/DefaultJsRuntime.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    * Response to review. Add custom exception and code cleanup
    
    * Remove unneded method
    
    * Update src/Mocking/JSInterop/MissingMockJsRuntimeException.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    * Update src/Mocking/JSInterop/MissingMockJsRuntimeException.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    * Update src/Mocking/JSInterop/MissingMockJsRuntimeException.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    * Update src/Mocking/JSInterop/MissingMockJsRuntimeException.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    * Update src/Mocking/JSInterop/MissingMockJsRuntimeException.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    * Update src/Mocking/JSInterop/MissingMockJsRuntimeException.cs
    
    Co-Authored-By: Egil Hansen <egil@assimilated.dk>
    
    Co-authored-by: Egil Hansen <egil@assimilated.dk>

---
+++ src/Mocking/JSInterop/PlaceholderJsRuntime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Egil.RazorComponents.Testing.Mocking.JSInterop
+{
+    /// <summary>
+    /// This JsRuntime is used to provide users with helpful exceptions if they fail to provide a mock when required. 
+    /// </summary>
+    internal class PlaceholderJsRuntime : IJSRuntime
+    {
+        public ValueTask<TValue> InvokeAsync<TValue>(string identifier, object[] args)
+        {
+            throw new MissingMockJsRuntimeException(identifier, args);
+        }
+
+        public ValueTask<TValue> InvokeAsync<TValue>(string identifier, CancellationToken cancellationToken, object[] args)
+        {
+            throw new MissingMockJsRuntimeException(identifier, args);
+        }
+    }
+}
---
+++ tests/TestServiceProviderTest.cs
@@ -0,0 +1,33 @@
+﻿using Egil.RazorComponents.Testing.EventDispatchExtensions;
+using Egil.RazorComponents.Testing.Extensions;
+using Egil.RazorComponents.Testing.Mocking.JSInterop;
+using Egil.RazorComponents.Testing.SampleComponents;
+using Egil.RazorComponents.Testing.SampleComponents.Data;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Egil.RazorComponents.Testing
+{
+    public class TestServiceProviderTest : ComponentTestFixture
+    {
+        [Fact(DisplayName = "The test service provider should register a placeholder IJSRuntime " +
+             "which throws exceptions")]
+        public void Test001()
+        {
+            var ex = Assert.Throws<AggregateException>(() => RenderComponent<SimpleWithJsRuntimeDep>());
+            Assert.True(ex?.InnerException is MissingMockJsRuntimeException);
+        }
+
+        [Fact(DisplayName = "The placeholder IJSRuntime is overriden by a supplied mock and does not throw")]
+        public void Test002()
+        {
+            Services.AddMockJsRuntime();
+
+            RenderComponent<SimpleWithJsRuntimeDep>();
+        }
+    }
+}
