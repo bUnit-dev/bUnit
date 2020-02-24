4b84be915a31c93dc9b42238eb54087ae582d6c2
Egil Hansen <egil@assimilated.dk>
2020-02-24 22:33:42 +0000

    Moved class to own file

--- src/Extensions/RenderWaitingHelperExtensions.cs
+++ src/Extensions/RenderWaitingHelperExtensions.cs
@@ -6,16 +6,6 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Bunit
 {
-    /// <summary>
-    /// Represents an exception that is thrown when a render does not happen within the specified wait period.
-    /// </summary>
-    public class WaitForRenderFailedException : Exception
-    {
-        private const string MESSAGE = "No render happened before the timeout period passed.";
-
-        /// <inheritdoc/>
-        public override string Message => MESSAGE;
-    }
 
     /// <summary>
     /// Helper methods dealing with async rendering during testing.
