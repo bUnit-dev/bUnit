e2288462a5914d9f0569f451c3adedd753d84c21
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-04-04 17:33:25 +0200

    wip...

---
+++ src/Builders/ComponentParameterBuilder.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bunit
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TComponent"></typeparam>
+    public sealed class ComponentParameterBuilder<TComponent> where TComponent : class, IComponent
+    {
+        private readonly ICollection<ComponentParameter> _componentParameters = new List<ComponentParameter>();
+
+        /// <summary>
+        /// Add a property or field-expression with normal value to this builder.
+        /// </summary>
+        /// <typeparam name="TValue">The generic Value type</typeparam>
+        /// <param name="expression">The property or field expression</param>
+        /// <param name="value">The value</param>
+        /// <returns>A <see cref="ComponentParameterBuilder&lt;TComponent&gt;"/> which can be chained.</returns>
+        public ComponentParameterBuilder<TComponent> Add<TValue>(Expression<Func<TComponent, TValue>> expression, TValue value)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+
+            AddParameterToList(ComponentParameter.CreateParameter(GetParameterName(expression), value));
+            return this;
+        }
+
+        /// <summary>
+        /// Add a property or field-expression with a cascading value to this builder.
+        /// </summary>
+        /// <typeparam name="TValue">The generic Value type</typeparam>
+        /// <param name="expression">The property or field expression</param>
+        /// <param name="value">The value</param>
+        /// <returns>A <see cref="ComponentParameterBuilder&lt;TComponent&gt;"/> which can be chained.</returns>
+        public ComponentParameterBuilder<TComponent> AddCascading<TValue>(Expression<Func<TComponent, TValue>> expression, TValue value)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            AddParameterToList(ComponentParameter.CreateCascadingValue(GetParameterName(expression), value));
+            return this;
+        }
+
+        /// <summary>
+        /// Add an unnamed cascading parameter with a value to this builder.
+        /// </summary>
+        /// <param name="value">The value</param>
+        /// <returns>A <see cref="ComponentParameterBuilder&lt;TComponent&gt;"/> which can be chained.</returns>
+        public ComponentParameterBuilder<TComponent> AddCascading(object value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            AddParameterToList(ComponentParameter.CreateCascadingValue(null, value));
+            return this;
+        }
+
+        public ComponentParameterBuilder<TComponent> Add(Expression<Func<TComponent, EventCallback>> expression, Func<Task> callback)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            AddParameterToList(ComponentParameter.CreateParameter(GetParameterName(expression), EventCallback.Factory.Create(this, callback)));
+            return this;
+        }
+
+        public ComponentParameterBuilder<TComponent> Add<TValue>(Expression<Func<TComponent, EventCallback<EventArgs>>> expression, Func<TValue, Task> callback)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            AddParameterToList(ComponentParameter.CreateParameter(GetParameterName(expression), EventCallback.Factory.Create(this, callback)));
+            return this;
+        }
+
+        private static string GetParameterName<TValue>(Expression<Func<TComponent, TValue>> expression)
+        {
+            if (expression.Body is MemberExpression memberExpression)
+            {
+                return memberExpression.Member.Name;
+            }
+
+            throw new ArgumentException($"The expression '{expression}' does not resolve to a Property or Field on the class '{typeof(TComponent)}'.");
+        }
+
+        private void AddParameterToList(ComponentParameter parameter)
+        {
+            if (!_componentParameters.Any(cp => string.Equals(cp.Name, parameter.Name, StringComparison.OrdinalIgnoreCase)))
+            {
+                _componentParameters.Add(parameter);
+            }
+            else
+            {
+                throw new ArgumentException($"A parameter with the name '{parameter.Name}' has already been added.");
+            }
+        }
+
+        /// <summary>
+        /// Create a <see cref="ComponentParameter"/> collection.
+        /// </summary>
+        /// <returns>A IReadOnlyCollection of <see cref="ComponentParameter"/></returns>
+        public IReadOnlyCollection<ComponentParameter> Build()
+        {
+            return _componentParameters.ToArray();
+        }
+    }
+}
---
+++ tests/Builders/ComponentParameterBuilderTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using Bunit.SampleComponents;
+using Microsoft.AspNetCore.Components;
+using Shouldly;
+using Xunit;
+
+namespace Bunit
+{
+    public class ComponentParameterBuilderTests
+    {
+        private readonly ComponentParameterBuilder<AllTypesOfParams<string>> _sut;
+
+        public ComponentParameterBuilderTests()
+        {
+            _sut = new ComponentParameterBuilder<AllTypesOfParams<string>>();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(42)]
+        public void Add_NullableInteger_And_Build_Should_Return_Correct_ReadonlyCollection(int? value)
+        {
+            // Arrange and Act
+            _sut.Add(c => c.NamedCascadingValue, value);
+            var result = _sut.Build();
+
+            // Assert
+            result.Count.ShouldBe(1);
+
+            var parameter = result.First();
+            parameter.Name.ShouldBe("NamedCascadingValue");
+            parameter.Value.ShouldBe(value);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("foo")]
+        public void Add_String_And_Build_Should_Return_Correct_ReadonlyCollection(string? value)
+        {
+            // Arrange and Act
+            _sut.Add(c => c.RegularParam, value);
+            var result = _sut.Build();
+
+            // Assert
+            result.Count.ShouldBe(1);
+
+            var parameter = result.First();
+            parameter.Name.ShouldBe("RegularParam");
+            parameter.Value.ShouldBe(value);
+        }
+
+        [Fact]
+        public void AddCascading_Integer_Without_Name_Return_Correct_ReadonlyCollection()
+        {
+            // Arrange
+            const int value = 42;
+            _sut.AddCascading(value);
+
+            // Act
+            var result = _sut.Build();
+
+            // Assert
+            result.Count.ShouldBe(1);
+
+            var parameter = result.First();
+            parameter.Name.ShouldBeNull();
+            parameter.Value.ShouldBe(value);
+        }
+
+        [Fact]
+        public void Add_Multiple_And_Build_Should_Return_Correct_ReadonlyCollection()
+        {
+            // Arrange and Act
+            _sut.Add(c => c.NamedCascadingValue, 42).Add(c => c.RegularParam, "bar");
+            var result = _sut.Build();
+
+            // Assert
+            result.Count.ShouldBe(2);
+
+            var first = result.First();
+            first.Name.ShouldBe("NamedCascadingValue");
+            first.Value.ShouldBe(42);
+
+            var second = result.Last();
+            second.Name.ShouldBe("RegularParam");
+            second.Value.ShouldBe("bar");
+        }
+
+        [Fact]
+        public void Add_GenericCallback_And_Build_Should_Return_Correct_ReadonlyCollection()
+        {
+            // Arrange
+            EventCallback<EventArgs> callback = EventCallback<EventArgs>.Empty;
+
+            // Act
+            _sut.Add(c => c.GenericCallback, callback);
+            var result = _sut.Build();
+
+            // Assert
+            result.Count.ShouldBe(1);
+
+            var parameter = result.First();
+            parameter.Name.ShouldBe("GenericCallback");
+            parameter.Value.ShouldBe(callback);
+        }
+
+        [Fact]
+        public void Add_Duplicate_Property_Should_Throw_Exception()
+        {
+            // Arrange
+            _sut.Add(c => c.NamedCascadingValue, null);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => _sut.Add(c => c.NamedCascadingValue, null));
+        }
+
+        [Fact]
+        public void AddCascading_With_NullValue_Should_Throw_Exception()
+        {
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => _sut.AddCascading(c => c.NamedCascadingValue, null));
+        }
+    }
+}
