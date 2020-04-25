2e29140c92f1046c7183ccd1e4630ce2d61a8767
Egil Hansen <egil@assimilated.dk>
2020-04-25 21:51:08 +0000

    Added discoverer tests

--- src/bunit.xunit.tests/RazorTesting/RazorTestDiscovererTest.cs
+++ src/bunit.xunit.tests/RazorTesting/RazorTestDiscovererTest.cs
@@ -1,84 +1,70 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using Bunit.SampleComponents;
-//using Moq;
-//using Shouldly;
-//using Xunit;
-//using Xunit.Abstractions;
-//using Xunit.Sdk;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Bunit.SampleComponents;
+using Moq;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
-//namespace Bunit.RazorTesting
-//{
-//	public class RazorTestDiscovererTest
-//	{
-//		private readonly ExceptionAggregator _aggregator;
-//		private readonly CancellationTokenSource _cancellationTokenSource;
-//		private readonly IMessageSink _messageBus;
-//		private readonly ITestFrameworkDiscoveryOptions _options;
-//		private readonly IReflectionAttributeInfo _attribute;
+namespace Bunit.RazorTesting
+{
+	public class RazorTestDiscovererTest
+	{
+		private readonly ExceptionAggregator _aggregator;
+		private readonly CancellationTokenSource _cancellationTokenSource;
+		private readonly IMessageSink _messageBus;
+		private readonly ITestFrameworkDiscoveryOptions _options;
+		private readonly IReflectionAttributeInfo _attribute;
 
-//		public RazorTestDiscovererTest()
-//		{
-//			_aggregator = new ExceptionAggregator();
-//			_cancellationTokenSource = new CancellationTokenSource();
-//			_options = TestFrameworkOptions.ForDiscovery();
-//			_messageBus = Mock.Of<IMessageSink>();
-//			_attribute = Mock.Of<IReflectionAttributeInfo>();
-//		}
+		public RazorTestDiscovererTest()
+		{
+			_aggregator = new ExceptionAggregator();
+			_cancellationTokenSource = new CancellationTokenSource();
+			_options = TestFrameworkOptions.ForDiscovery();
+			_messageBus = Mock.Of<IMessageSink>();
+			_attribute = Mock.Of<IReflectionAttributeInfo>();
+		}
 
-//		[Fact]
-//		public void MyTestMethod()
-//		{
-//			var discoverer = new RazorTestDiscoverer(_messageBus);
-//			var testMethod = Mocks.TestMethod(typeof(OneFixtureComponent), nameof(OneFixtureComponent.Test));
+		[Fact(DisplayName = "Can find single razor test in test component")]
+		public void Test001()
+		{
+			var discoverer = new RazorTestDiscoverer(_messageBus);
+			var testMethod = Mocks.TestMethod(typeof(OneFixtureComponent), nameof(OneFixtureComponent.RazorTests));
 
-//			var testCases = discoverer.Discover(_options, testMethod, _attribute);
+			var testCases = discoverer.Discover(_options, testMethod, _attribute);
 
-//			testCases.ShouldHaveSingleItem()
-//				.ShouldBeOfType<RazorTestCase>()
-//				.DisplayName.ShouldBe("DESCRIPTION");
-//		}
+			testCases.ShouldAllBe(x => x.DisplayName.ShouldBe("FIXTURE 1"));
+		}
 
+		[Fact(DisplayName = "Can find two razor test in test component")]
+		public void Test002()
+		{
+			var discoverer = new RazorTestDiscoverer(_messageBus);
+			var testMethod = Mocks.TestMethod(typeof(TwoFixtureComponent), nameof(TwoFixtureComponent.RazorTests));
 
-//	}
+			var testCases = discoverer.Discover(_options, testMethod, _attribute);
 
-//	public static class Mocks
-//	{
-//		public static TestMethod TestMethod(Type type, string methodName, ITestCollection? collection = null)
-//		{
-//			var @class = TestClass(type, collection);
-//			var methodInfo = type.GetMethod(methodName);
-//			if (methodInfo == null)
-//				throw new Exception($"Unknown method: {type.FullName}.{methodName}");
+			testCases.ShouldAllBe(
+				x => x.DisplayName.ShouldBe("FIXTURE 1"),
+				x => x.DisplayName.ShouldBe("FIXTURE 2")
+			);
+		}
 
-//			return new TestMethod(@class, Reflector.Wrap(methodInfo));
-//		}
+		[Fact(DisplayName = "Can find zero razor test in test component")]
+		public void Test003()
+		{
+			var discoverer = new RazorTestDiscoverer(_messageBus);
+			var testMethod = Mocks.TestMethod(typeof(ZeroFixtureComponent), nameof(ZeroFixtureComponent.RazorTests));
 
-//		public static TestClass TestClass(Type type, ITestCollection? collection = null)
-//		{
-//			if (collection == null)
-//				collection = TestCollection(type.GetTypeInfo().Assembly);
+			var testCases = discoverer.Discover(_options, testMethod, _attribute);
 
-//			return new TestClass(collection, Reflector.Wrap(type));
-//		}
+			testCases.ShouldBeEmpty();
+		}
 
-//		public static TestCollection TestCollection(Assembly? assembly = null, ITypeInfo? definition = null, string? displayName = null)
-//		{
-//			if (assembly == null)
-//				assembly = typeof(Mocks).GetTypeInfo().Assembly;
-//			if (displayName == null)
-//				displayName = "Mock test collection for " + assembly.CodeBase;
-
-//			return new TestCollection(TestAssembly(assembly), definition, displayName);
-//		}
-//		public static TestAssembly TestAssembly(Assembly? assembly = null, string? configFileName = null)
-//		{
-//			return new TestAssembly(Reflector.Wrap(assembly ?? typeof(Mocks).GetTypeInfo().Assembly), configFileName);
-//		}
-//	}
-//}
+		// TODO: When rendering of test component fails an ExecutionErrorTestCase is returned
+	}
+}
--- src/bunit.xunit.tests/SampleComponents/OneFixtureComponent.cs
+++ src/bunit.xunit.tests/SampleComponents/OneFixtureComponent.cs
@@ -1,16 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace Bunit.SampleComponents
 {
 	public class OneFixtureComponent : TestComponentBase
-	{		
+	{
 		protected override void BuildRenderTree(RenderTreeBuilder builder)
 		{
 			builder.OpenComponent<Fixture>(0);
-			builder.AddAttribute(1, nameof(Fixture.Description), "Fixture name");
-			//builder.AddAttribute(2, nameof(Fixture.ChildContent), (RenderFragment)Content);
+			builder.AddAttribute(1, nameof(Fixture.Description), "FIXTURE 1");
+			builder.AddAttribute(2, nameof(Fixture.ChildContent), (RenderFragment)Content);
+			builder.AddAttribute(3, nameof(Fixture.Test), (Action<Fixture>)TestMethod);
 			builder.CloseComponent();
 		}
+
+		private void TestMethod(Fixture fixture) { }
 
 		private void Content(RenderTreeBuilder builder) { }
 	}
