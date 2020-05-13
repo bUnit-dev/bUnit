d9bf80ef49efd186b9985a39cdd886b35fd34c74
Egil Hansen <egil@assimilated.dk>
2020-05-13 09:31:37 +0000

    Update to dependencies, updates to template

---
+++ templates/bunit-project/template/CounterCSharpTest.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+using Bunit;
+using Bunit.Mocking.JSInterop;
+using static Bunit.ComponentParameterFactory;
+
+namespace Company.BlazorTests1
+{
+    /// <summary>
+    /// These tests are written entirely in C#.
+    /// Learn more at https://bunit.egilhansen.com/docs/
+    /// </summary>
+    public class CounterCSharpTests : TestContext
+    {
+        [Fact]
+        public void CounterStartsAtZero()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Assert that content of the paragraph shows counter at zero
+            cut.Find("p").MarkupMatches("<p>Current count: 0</p>"); 
+        }
+
+        [Fact]
+        public void ClickingButtonIncrementsCounter()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Act - click button to increment counter
+            cut.Find("button").Click();
+
+            // Assert that the counter was incremented
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
