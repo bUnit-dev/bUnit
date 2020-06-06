1c7dedd24c2cc8a90a93215469e731fac3668727
Egil Hansen <egil@assimilated.dk>
2020-06-06 23:41:15 +0000

    Docs: Trigger event handlers

---
+++ docs/samples/tests/xunit/ClickMeTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components.Web;
+using Xunit;
+using Bunit;
+
+namespace Bunit.Docs.Samples
+{
+  public class ClickMeTest
+  {
+    [Fact]
+    public void ClickingButtonWorks()
+    {
+      // Arrange
+      using var ctx = new TestContext();
+      var cut = ctx.RenderComponent<ClickMe>();
+      var buttonElement = cut.Find("button");
+
+      // Act
+      buttonElement.Click();
+      buttonElement.Click(detail: 3, ctrlKey: true);
+      buttonElement.Click(new MouseEventArgs());
+
+      // Assert
+      // ...
+    }
+  }
+}
