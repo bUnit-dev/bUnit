16b7585bc8a4e4b26b5001ace92e2e881622f68d
Pedro Silva <DarthPedro@gmail.com>
2020-06-15 17:33:00 -0700

    Added extension methods to add appropriate authentication/authorization test service, in the right state, to the TestServiceProvider. Added unit tests.

---
+++ src/bunit.web.tests/TestDoubles/Authorization/FakeAuthorizationExtensionsTest.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Bunit.TestDoubles.Authorization
+{
+	public class FakeAuthorizationExtensionsTest
+	{
+		[Fact(DisplayName = "Register Authorization services with authenticated and authorized user.")]
+		public async Task Test001()
+		{
+			// arrange
+			using var sp = new TestServiceProvider();
+			var roles = new List<string> { "User" };
+			var principal = new ClaimsPrincipal(
+				new FakePrincipal { Identity = new FakeIdentity { Name = "DarthPedro" }, Roles = roles });
+
+			// act
+			sp.AddAuthorization("DarthPedro", true, roles);
+
+			var authProvider = sp.GetRequiredService<AuthenticationStateProvider>();
+			var authState = await authProvider.GetAuthenticationStateAsync();
+
+			var authService = sp.GetRequiredService<IAuthorizationService>();
+			var authResult = await authService.AuthorizeAsync(principal, "testResource", "testPolicy");
+
+			// assert
+			Assert.NotNull(authState.User);
+			Assert.NotNull(authState.User.Identity);
+			Assert.Equal("DarthPedro", authState.User.Identity.Name);
+			Assert.True(authState.User.Identity.IsAuthenticated);
+			Assert.True(authResult.Succeeded);
+		}
+
+		[Fact(DisplayName = "Register Authorization services with authenticated but unauthorized user.")]
+		public async Task Test002()
+		{
+			// arrange
+			using var sp = new TestServiceProvider();
+			var principal = new ClaimsPrincipal(
+				new FakePrincipal { Identity = new FakeIdentity { Name = "DarthPedro" } });
+
+			// act
+			sp.AddAuthorization("DarthPedro", false);
+
+			var authProvider = sp.GetRequiredService<AuthenticationStateProvider>();
+			var authState = await authProvider.GetAuthenticationStateAsync();
+
+			var authService = sp.GetRequiredService<IAuthorizationService>();
+			var authResult = await authService.AuthorizeAsync(principal, "testResource", "testPolicy");
+
+			// assert
+			Assert.NotNull(authState.User);
+			Assert.NotNull(authState.User.Identity);
+			Assert.Equal("DarthPedro", authState.User.Identity.Name);
+			Assert.True(authState.User.Identity.IsAuthenticated);
+			Assert.False(authResult.Succeeded);
+		}
+
+		[Fact(DisplayName = "Register Authorization services with unauthenticated user.")]
+		public async Task Test003()
+		{
+			// arrange
+			using var sp = new TestServiceProvider();
+			var principal = new ClaimsPrincipal(
+				new FakePrincipal { Identity = new FakeIdentity { Name = "DarthPedro" } });
+
+			// act
+			sp.AddAuthorization();
+
+			var authProvider = sp.GetRequiredService<AuthenticationStateProvider>();
+			var authState = await authProvider.GetAuthenticationStateAsync();
+
+			var authService = sp.GetRequiredService<IAuthorizationService>();
+			var authResult = await authService.AuthorizeAsync(principal, "testResource", "testPolicy");
+
+			// assert
+			Assert.NotNull(authState.User);
+			Assert.NotNull(authState.User.Identity);
+			Assert.Null(authState.User.Identity.Name);
+			Assert.False(authState.User.Identity.IsAuthenticated);
+			Assert.False(authResult.Succeeded);
+		}
+	}
+}
---
+++ src/bunit.web/TestDoubles/Authorization/FakeAuthorizationExtensions.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bunit.TestDoubles.Authorization
+{
+	/// <summary>
+	/// Helper methods for registering the Authentication/Authorization services with a <see cref="TestServiceProvider"/>.
+	/// </summary>
+	public static class FakeAuthorizationExtensions
+	{
+		/// <summary>
+		/// Adds the appropriate auth services to the <see cref="TestServiceProvider"/> to enable an authenticated user.
+		/// </summary>
+		public static void AddAuthorization(this TestServiceProvider serviceProvider, string userName, bool isAuthorized = true, IList<string>? roles = null)
+		{
+			var authService = new FakeAuthorizationService()
+			{
+				NextResult = isAuthorized ? AuthorizationResult.Success() : AuthorizationResult.Failed(),
+			};
+
+			serviceProvider.AddSingleton<IAuthorizationService>(authService);
+			serviceProvider.AddSingleton<IAuthorizationPolicyProvider>(new FakeAuthorizationPolicyProvider());
+			serviceProvider.AddSingleton<AuthenticationStateProvider>(new FakeAuthenticationStateProvider(userName, roles));
+		}
+
+		/// <summary>
+		/// Adds the appropriate auth services to the <see cref="TestServiceProvider"/> to enable an authenticated user.
+		/// </summary>
+		public static void AddAuthorization(this TestServiceProvider serviceProvider)
+		{
+			var authService = new FakeAuthorizationService()
+			{
+				NextResult = AuthorizationResult.Failed()
+			};
+
+			serviceProvider.AddSingleton<IAuthorizationService>(authService);
+			serviceProvider.AddSingleton<IAuthorizationPolicyProvider>(new FakeAuthorizationPolicyProvider());
+			serviceProvider.AddSingleton<AuthenticationStateProvider>(new FakeAuthenticationStateProvider());
+		}
+	}
+}
