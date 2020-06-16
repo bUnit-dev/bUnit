f92a850265cbacad97c073a663cdf94c6b9a4379
Pedro Silva <DarthPedro@gmail.com>
2020-06-16 09:36:33 -0700

    Added component tests that use AuthorizeView and the BUnit authorization fakes to validate the various states.

---
+++ src/bunit.web.tests/TestDoubles/Authorization/LoginDisplayTest.cs
@@ -0,0 +1,58 @@
+using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Bunit.TestDoubles.Authorization
+{
+	public class LoginDisplayTest
+	{
+		private readonly Mock<NavigationManager> navManager = new Mock<NavigationManager>();
+
+		[Fact(DisplayName = "Login AuthorizeView with unauthenticated user")]
+		public void Test001()
+		{
+			// Arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization();
+			ctx.Services.AddSingleton<NavigationManager>(this.navManager.Object);
+
+			// Act
+			var cut = ctx.RenderComponent<LoginDisplay>();
+
+			// Assert
+			cut.MarkupMatches(@"<a href=""authentication/login"">Log in</a>");
+		}
+
+		[Fact(DisplayName = "Login AuthorizeView with authenticated and authorized user")]
+		public void Test002()
+		{
+			// arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization("TestUser", true);
+			ctx.Services.AddSingleton<NavigationManager>(this.navManager.Object);
+
+			// act
+			var cut = ctx.RenderComponent<LoginDisplay>();
+
+			// assert
+			Assert.Contains("Hello, TestUser!", cut.Markup, StringComparison.InvariantCulture);
+		}
+
+		[Fact(DisplayName = "Login AuthorizeView with authenticated but unauthorized user")]
+		public void Test003()
+		{
+			// arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization("TestUser", false);
+			ctx.Services.AddSingleton<NavigationManager>(this.navManager.Object);
+
+			// act
+			var cut = ctx.RenderComponent<LoginDisplay>();
+
+			// assert
+			cut.MarkupMatches(@"<a href=""authentication/login"">Log in</a>");
+		}
+	}
+}
---
+++ src/bunit.web.tests/TestDoubles/Authorization/SimpleAuthViewTest.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Bunit.TestDoubles.Authorization
+{
+	public class SimpleAuthViewTest
+	{
+		[Fact(DisplayName = "AuthorizeView with unauthenticated user")]
+		public void Test001()
+		{
+			// Arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization();
+
+			// Act
+			var cut = ctx.RenderComponent<SimpleAuthView>();
+
+			// Assert
+			cut.MarkupMatches("Not authorized?");
+		}
+
+		[Fact(DisplayName = "AuthorizeView with authenticated and authorized user")]
+		public void Test002()
+		{
+			// arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization("TestUser", true);
+
+			// act
+			var cut = ctx.RenderComponent<SimpleAuthView>();
+
+			// assert
+			cut.MarkupMatches("Authorized!");
+		}
+
+		[Fact(DisplayName = "AuthorizeView with authenticated but unauthorized user")]
+		public void Test003()
+		{
+			// arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization("TestUser", false);
+
+			// act
+			var cut = ctx.RenderComponent<SimpleAuthView>();
+
+			// assert
+			cut.MarkupMatches("Not authorized?");
+		}
+
+		[Fact(DisplayName = "AuthorizeView switch from unauthenticated to authenticated.")]
+		public void Test004()
+		{
+			// arrange
+			using var ctx = new TestContext();
+			ctx.Services.AddAuthorization();
+
+			// start off unauthenticated.
+			var cut = ctx.RenderComponent<SimpleAuthView>();
+			cut.MarkupMatches("Not authorized?");
+
+			// act
+			var authProvider = (FakeAuthenticationStateProvider)ctx.Services.GetRequiredService<AuthenticationStateProvider>();
+			var newState = FakeAuthenticationStateProvider.CreateAuthenticationState("TestUser004");
+			authProvider?.TriggerAuthenticationStateChanged(newState);
+
+			var authService = (FakeAuthorizationService)ctx.Services.GetRequiredService<IAuthorizationService>();
+			authService.NextResult = AuthorizationResult.Success();
+
+			cut.Render();
+
+			// assert
+			cut.MarkupMatches("Authorized!");
+		}
+	}
+}
