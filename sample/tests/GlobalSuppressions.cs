7faed21f107eda43f6b5288d06951209784e4efd
Egil Hansen <egil@assimilated.dk>
2020-08-14 14:11:42 +0000

    Removed old samples

--- sample/tests/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>", Scope = "member", Target = "~M:Bunit.SampleApp.Tests.Components.AlertTest2.Test008~System.Threading.Tasks.Task")]
