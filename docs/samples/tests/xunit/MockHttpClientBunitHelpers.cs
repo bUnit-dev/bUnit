474243a2eee970c638c0df8771cd08bb8833b987
Egil Hansen <egil@assimilated.dk>
2020-10-04 15:26:43 +0000

    Docs: added httpclient mocking guide to docs

---
+++ docs/samples/tests/xunit/MockHttpClientBunitHelpers.cs
@@ -0,0 +1,47 @@
+namespace Bunit.Docs.Samples
+{
+    using Bunit;
+    using Microsoft.Extensions.DependencyInjection;
+    using RichardSzalay.MockHttp;
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text.Json;
+
+    public static class MockHttpClientBunitHelpers
+	{
+		public static MockHttpMessageHandler AddMockHttpClient(this TestServiceProvider services)
+		{
+			var mockHttpHandler = new MockHttpMessageHandler();
+			var httpClient = mockHttpHandler.ToHttpClient();
+			httpClient.BaseAddress = new Uri("http://localhost");
+			services.AddSingleton<HttpClient>(httpClient);
+			return mockHttpHandler;
+		}
+
+		public static MockedRequest RespondJson<T>(this MockedRequest request, T content)
+		{
+			request.Respond(req =>
+			{
+				var response = new HttpResponseMessage(HttpStatusCode.OK);
+				response.Content = new StringContent(JsonSerializer.Serialize(content));
+				response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+				return response;
+			});
+			return request;
+		}
+
+		public static MockedRequest RespondJson<T>(this MockedRequest request, Func<T> contentProvider)
+		{
+			request.Respond(req =>
+			{
+				var response = new HttpResponseMessage(HttpStatusCode.OK);
+				response.Content = new StringContent(JsonSerializer.Serialize(contentProvider()));
+				response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+				return response;
+			});
+			return request;
+		}
+	}
+}
