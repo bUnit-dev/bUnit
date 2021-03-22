66110fe76b9d3003d4711c413eec4e36626f67e8
Egil Hansen <egil@assimilated.dk>
2021-03-22 15:52:26 +0000

    Created simple FakeNavigationManager that is registered by default with bUnit's services provider

---
+++ tests/bunit.web.tests/TestDoubles/NavigationManager/FakeNavigationManagerTest.cs
@@ -0,0 +1,93 @@
+using System;
+using Bunit.TestAssets.SampleComponents;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Bunit.TestDoubles
+{
+	public class FakeNavigationManagerTest : TestContext
+	{
+		private NavigationManager CreateFakeNavigationMananger()
+			=> Services.GetRequiredService<NavigationManager>();
+
+		[Fact(DisplayName = "TestContext.Services has NavigationManager registered by default as FakeNavigationManager")]
+		public void Test001()
+		{
+			Services.GetService<NavigationManager>()
+				.ShouldBeOfType<FakeNavigationManager>();
+		}
+
+		[Fact(DisplayName = "FakeNavigationManager.BaseUrl is set to http://localhost/")]
+		public void Test002()
+		{
+			var sut = CreateFakeNavigationMananger();
+
+			sut.BaseUri.ShouldBe("http://localhost/");
+		}
+
+		[Theory(DisplayName = "NavigateTo with relative URI converts it to absolute and sets the Uri property ")]
+		[InlineData("")]
+		[InlineData("/")]
+		[InlineData("/foo")]
+		public void Test003(string uri)
+		{
+			var sut = CreateFakeNavigationMananger();
+			var expectedUri = new Uri(new Uri(sut.BaseUri, UriKind.Absolute), new Uri(uri, UriKind.Relative));
+
+			sut.NavigateTo(uri);
+
+			sut.Uri.ShouldBe(expectedUri.ToString());
+		}
+
+		[Theory(DisplayName = "NavigateTo with absolute URI sets the Uri property ")]
+		[InlineData("http://localhost")]
+		[InlineData("http://localhost/")]
+		[InlineData("http://localhost/foo")]
+		public void Test004(string uri)
+		{
+			var sut = CreateFakeNavigationMananger();
+			var expectedUri = new Uri(uri, UriKind.Absolute);
+
+			sut.NavigateTo(uri);
+
+			sut.Uri.ShouldBe(expectedUri.ToString());
+		}
+
+		[Fact(DisplayName = "NavigateTo raises the NotifyLocationChanged")]
+		public void Test005()
+		{
+			// arrange
+			LocationChangedEventArgs actualLocationChange = default;
+			var navigationUri = "foo";
+			var sut = CreateFakeNavigationMananger();
+			sut.LocationChanged += Sut_LocationChanged;
+
+			// act
+			sut.NavigateTo(navigationUri);
+
+			// assert
+			actualLocationChange.Location.ShouldBe($"{sut.BaseUri}{navigationUri}");
+			actualLocationChange.IsNavigationIntercepted.ShouldBeFalse();
+
+			// test helpers
+			void Sut_LocationChanged(object? sender, LocationChangedEventArgs e)
+			{
+				actualLocationChange = e;
+			}
+		}
+
+		[Fact(DisplayName = "LocationChanged is raised on the test renderer's dispatcher")]
+		public void Test006()
+		{
+			var sut = CreateFakeNavigationMananger();
+			var cut = RenderComponent<PrintCurrentUrl>();
+
+			sut.NavigateTo("foo");
+
+			cut.Markup.ShouldBe($"{sut.BaseUri}foo");
+		}
+	}
+}
