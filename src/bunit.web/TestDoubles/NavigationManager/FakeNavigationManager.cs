01a09d92e57e8815a58767516c0b06c58a8ed5c4
Egil Hansen <egil@assimilated.dk>
2021-03-22 15:52:26 +0000

    Created simple FakeNavigationManager that is registered by default with bUnit's services provider

---
+++ src/bunit.web/TestDoubles/NavigationManager/FakeNavigationManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit.Rendering;
+using Microsoft.AspNetCore.Components;
+
+namespace Bunit.TestDoubles
+{
+	/// <summary>
+	/// Represents a fake <see cref="NavigationManager"/> that captures calls to
+	/// <see cref="NavigationManager.NavigateTo(string, bool)"/> for testing purposes.
+	/// </summary>
+	public sealed class FakeNavigationManager : NavigationManager
+	{
+		private readonly ITestRenderer renderer;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FakeNavigationManager"/> class.
+		/// </summary>
+		[SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "By design. Fake navigation manager defaults to local host as base URI.")]
+		public FakeNavigationManager(ITestRenderer renderer)
+		{
+			this.renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
+			Initialize("http://localhost/", "http://localhost/");
+		}
+
+		/// <inheritdoc/>
+		protected override void NavigateToCore(string uri, bool forceLoad)
+		{
+			Uri = ToAbsoluteUri(uri).ToString();
+
+			renderer.Dispatcher.InvokeAsync(
+				() => NotifyLocationChanged(isInterceptedLink: false));
+		}
+	}
+}
