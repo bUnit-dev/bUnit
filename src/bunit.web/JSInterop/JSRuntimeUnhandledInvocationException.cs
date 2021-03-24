3e53e656ba62bfd60ca307efc49c5e54451374f9
Egil Hansen <egil@assimilated.dk>
2021-03-24 23:33:03 +0000

    Added setup suggestions to JSRuntimeUnhandledInvocationException message

--- src/bunit.web/JSInterop/JSRuntimeUnhandledInvocationException.cs
+++ src/bunit.web/JSInterop/JSRuntimeUnhandledInvocationException.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Text;
 
 namespace Bunit
 {
@@ -24,24 +24,102 @@ namespace Bunit
 		/// </summary>
 		/// <param name="invocation">The unplanned invocation.</param>
 		public JSRuntimeUnhandledInvocationException(JSRuntimeInvocation invocation)
-			: base($"The invocation of '{invocation.Identifier}' {PrintArguments(invocation.Arguments)} was not expected.")
+			: base(CreateErrorMessage(invocation))
 		{
 			Invocation = invocation;
-		}
-
-		private static string PrintArguments(IReadOnlyList<object?> arguments)
-		{
-			if (arguments.Count == 0)
-				return "without arguments";
-			if (arguments.Count == 1)
-				return $"with the argument [{arguments[0]}]";
-
-			return $"with arguments [{string.Join(", ", arguments.OfType<object>().Select(x => x.ToString()))}]";
 		}
 
 		private JSRuntimeUnhandledInvocationException(SerializationInfo serializationInfo, StreamingContext streamingContext)
 			: base(serializationInfo, streamingContext)
 		{
+		}
+
+		private static string CreateErrorMessage(JSRuntimeInvocation invocation)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("bUnit's JSInterop has not been configured to handle the call:");
+			sb.AppendLine();
+
+			if (invocation.IsVoidResultInvocation)
+			{
+				sb.AppendLine($"    {invocation.InvocationMethodName}({GetArguments(invocation)})");
+			}
+			else
+			{
+				sb.AppendLine($"    {invocation.InvocationMethodName}<{GetGenericInvocationArguments(invocation)}>({GetArguments(invocation)})");
+			}
+
+			sb.AppendLine();
+			sb.AppendLine("Configure bUnit's JSInterop to handle the call with following:");
+			sb.AppendLine();
+
+			if (invocation.IsVoidResultInvocation)
+			{
+				sb.AppendLine($"    SetupVoid({GetArguments(invocation)})");
+			}
+			else
+			{
+				sb.AppendLine($"    Setup<{GetReturnTypeName(invocation.ResultType)}>({GetArguments(invocation)})");
+			}
+
+			if (invocation.Arguments.Any())
+			{
+				sb.AppendLine("or the following, to match any arguments:");
+				if (invocation.IsVoidResultInvocation)
+					sb.AppendLine($"    SetupVoid(\"{invocation.Identifier}\", _ => true)");
+				else
+					sb.AppendLine($"    Setup<{GetReturnTypeName(invocation.ResultType)}>(\"{invocation.Identifier}\", _ => true)");
+			}
+
+			sb.AppendLine();
+			sb.AppendLine("The setup methods are available on an instance of the BunitJSInterop or");
+			sb.AppendLine("BunitJSModuleInterop type. The standard BunitJSInterop is available");
+			sb.AppendLine("through the TestContext.JSInterop property, and a BunitJSModuleInterop");
+			sb.AppendLine("instance is returned from calling SetupModule on a BunitJSInterop instance.");
+			return sb.ToString();
+		}
+
+		private static string GetReturnTypeName(Type resultType)
+			=> resultType switch
+			{
+				Type { FullName: "System.Boolean" } => "bool",
+				Type { FullName: "System.Byte" } => "byte",
+				Type { FullName: "System.Char" } => "char",
+				Type { FullName: "System.Double" } => "double",
+				Type { FullName: "System.Int16" } => "short",
+				Type { FullName: "System.Int32" } => "int",
+				Type { FullName: "System.Int64" } => "long",
+				Type { FullName: "System.SByte" } => "sbyte",
+				Type { FullName: "System.Single" } => "float",
+				Type { FullName: "System.UInt16" } => "ushort",
+				Type { FullName: "System.UInt32" } => "uint",
+				Type { FullName: "System.UInt64" } => "ulong",
+				Type x => x.Name
+			};
+
+		private static string GetGenericInvocationArguments(JSRuntimeInvocation invocation)
+		{
+			if (invocation.InvocationMethodName.Equals("InvokeUnmarshalled", StringComparison.Ordinal))
+			{
+				var genericArgs = invocation.Arguments
+					.Select(x => x is not null ? GetReturnTypeName(x.GetType()) : "?")
+					.Append(GetReturnTypeName(invocation.ResultType));
+
+				return string.Join(", ", genericArgs);
+			}
+			else
+			{
+				return GetReturnTypeName(invocation.ResultType);
+			}
+		}
+
+		private static string GetArguments(JSRuntimeInvocation invocation)
+		{
+			var args = invocation.Arguments
+				.Select(x => x is string s ? $"\"{s}\"" : x?.ToString() ?? "null")
+				.Prepend($"\"{invocation.Identifier}\"");
+
+			return string.Join(", ", args);
 		}
 	}
 }
