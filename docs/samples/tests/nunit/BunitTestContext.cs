f161b19ada4a3d1ee1524bba4bfd3dafd992db61
Egil Hansen <egil@assimilated.dk>
2021-04-06 23:51:55 +0000

    Updated docs article on writing tests in .cs and .razor files

--- docs/samples/tests/nunit/BunitTestContext.cs
+++ docs/samples/tests/nunit/BunitTestContext.cs
@@ -1,35 +1,14 @@
-using System;
 using Bunit;
-using Bunit.Rendering;
-using Microsoft.AspNetCore.Components;
 using NUnit.Framework;
 
 namespace Bunit.Docs.Samples
 {
-  public abstract class BunitTestContext : IDisposable
-  {
-    private Bunit.TestContext _context;
-
-    public ITestRenderer Renderer => _context?.Renderer ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
-
-    public TestServiceProvider Services => _context?.Services ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
-
-    public void Dispose()
+  public abstract class BunitTestContext : TestContextWrapper
     {
-      _context?.Dispose();
-      _context = null;
-    }
-
     [SetUp]
-    public void Setup() => _context = new Bunit.TestContext();
+    public void Setup() => TestContext = new Bunit.TestContext();
 
     [TearDown]
-    public void TearDown() => Dispose();
-
-    public IRenderedComponent<TComponent> RenderComponent<TComponent>(params ComponentParameter[] parameters) where TComponent : IComponent
-        => _context?.RenderComponent<TComponent>(parameters) ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
-
-    public IRenderedComponent<TComponent> RenderComponent<TComponent>(Action<ComponentParameterCollectionBuilder<TComponent>> parameterBuilder) where TComponent : IComponent
-        => _context?.RenderComponent<TComponent>(parameterBuilder) ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
+    public void TearDown() => TestContext?.Dispose();
   }
 }
