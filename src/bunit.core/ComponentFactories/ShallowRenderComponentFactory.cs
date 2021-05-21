72d9eb290b985b8c70fcd3866ebe8c1c7be29628
Egil Hansen <egil@assimilated.dk>
2021-05-21 19:41:23 +0000

    refactor: ShallowRenderComponentFactory using ComponentDoubleFactory.CreateStub

--- src/bunit.core/ComponentFactories/ShallowRenderComponentFactory.cs
+++ src/bunit.core/ComponentFactories/ShallowRenderComponentFactory.cs
@@ -2,6 +2,7 @@
 using System;
 using Bunit.Rendering;
 using Bunit.TestDoubles;
+using Bunit.TestDoubles.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace Bunit.ComponentFactories
@@ -11,9 +12,10 @@ namespace Bunit.ComponentFactories
 	/// all components using the <see cref="Stub{TComponent}"/>,
 	/// except the first child component of the <see cref="ShallowRenderContainer"/>.
 	/// </summary>
-	internal class ShallowRenderComponentFactory : IComponentFactory
+	internal sealed class ShallowRenderComponentFactory : IComponentFactory
 	{
 		private static readonly Type ShallowRenderContainerType = typeof(ShallowRenderContainer);
+		private static readonly StubOptions DefaultStubOptions = StubOptions.Default;
 		private bool shallowRenderContainerSeen;
 		private bool hasCreatedComponentUnderTest;
 
@@ -36,8 +38,7 @@ namespace Bunit.ComponentFactories
 		{
 			if (hasCreatedComponentUnderTest)
 			{
-				var stubType = typeof(Stub<>).MakeGenericType(componentType);
-				return (IComponent)Activator.CreateInstance(stubType)!;
+				return ComponentDoubleFactory.CreateStub(componentType, DefaultStubOptions);
 			}
 
 			hasCreatedComponentUnderTest = true;
