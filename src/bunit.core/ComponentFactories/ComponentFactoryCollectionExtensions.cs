7517f34a7a4cc1bf7a05f19481d79e860848cd3b
Egil Hansen <egil@assimilated.dk>
2021-05-21 22:16:39 +0000

    feat: added UseFor generic replacement helper method

--- src/bunit.core/ComponentFactories/ComponentFactoryCollectionExtensions.cs
+++ src/bunit.core/ComponentFactories/ComponentFactoryCollectionExtensions.cs
@@ -80,6 +80,26 @@ namespace Bunit
 			factories.Add(new StubComponentFactory(componentTypePredicate, options ?? StubOptions.Default));
 			return factories;
 		}
+
+		/// <summary>
+		/// Configures bUnit to replace all components of type <typeparamref name="TComponent"/> with a component
+		/// of type <typeparamref name="TReplacementComponent"/>.
+		/// </summary>
+		/// <typeparam name="TComponent">Type of component to replace.</typeparam>
+		/// <typeparam name="TReplacementComponent">Type of component to replace with.</typeparam>
+		/// <param name="factories">The bUnit <see cref="ComponentFactoryCollection"/> to configure.</param>
+		/// <returns>A <see cref="ComponentFactoryCollection"/>.</returns>
+		public static ComponentFactoryCollection UseFor<TComponent, TReplacementComponent>(this ComponentFactoryCollection factories)
+			where TComponent : IComponent
+			where TReplacementComponent : IComponent
+		{
+			if (factories is null)
+				throw new ArgumentNullException(nameof(factories));
+
+			factories.Add(new GenericComponentFactory<TComponent, TReplacementComponent>());
+
+			return factories;
+		}
 	}
 }
 #endif
