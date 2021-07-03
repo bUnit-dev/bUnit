85f77b5d2c40f6d2329e522973c833bf71749c15
Javier Goday <jgoday@gmail.com>
2021-07-03 13:36:25 +0200

    fix tabs

--- src/bunit.web/JSInterop/InvocationHandlers/JSRuntimeInvocationHandlerFactory.cs
+++ src/bunit.web/JSInterop/InvocationHandlers/JSRuntimeInvocationHandlerFactory.cs
@@ -3,29 +3,29 @@ using System.Threading.Tasks;
 
 namespace Bunit.JSInterop.InvocationHandlers
 {
-    internal class JSRuntimeInvocationHandlerFactory<TResult, TException> : JSRuntimeInvocationHandler<TResult>
-        where TException : Exception
-    {
-        private readonly Func<JSRuntimeInvocation, TResult>? resultFactory;
-        private readonly Func<JSRuntimeInvocation, TException>? exceptionFactory;
+	internal class JSRuntimeInvocationHandlerFactory<TResult, TException> : JSRuntimeInvocationHandler<TResult>
+		where TException : Exception
+	{
+		private readonly Func<JSRuntimeInvocation, TResult>? resultFactory;
+		private readonly Func<JSRuntimeInvocation, TException>? exceptionFactory;
 
-        public JSRuntimeInvocationHandlerFactory(Func<JSRuntimeInvocation, TResult>? rFactory, Func<JSRuntimeInvocation, TException>? eFactory, InvocationMatcher matcher, bool isCatchAllHandler) : base(matcher, isCatchAllHandler)
-        {
-            resultFactory = rFactory;
-            exceptionFactory = eFactory;
-        }
-
-        protected override internal Task<TResult> HandleAsync(JSRuntimeInvocation invocation)
+		public JSRuntimeInvocationHandlerFactory(Func<JSRuntimeInvocation, TResult>? rFactory, Func<JSRuntimeInvocation, TException>? eFactory, InvocationMatcher matcher, bool isCatchAllHandler) : base(matcher, isCatchAllHandler)
 		{
-            if (exceptionFactory != null && exceptionFactory.Invoke(invocation) is TException t)
-            {
-                base.SetException<TException>(t);
-            }
-            else if (resultFactory != null && resultFactory.Invoke(invocation) is TResult res)
-            {
-                base.SetResult(res);
-            }
-            return base.HandleAsync(invocation);
+			resultFactory = rFactory;
+			exceptionFactory = eFactory;
 		}
-    }
+
+		protected override internal Task<TResult> HandleAsync(JSRuntimeInvocation invocation)
+		{
+			if (exceptionFactory != null && exceptionFactory.Invoke(invocation) is TException t)
+			{
+				base.SetException<TException>(t);
+			}
+			else if (resultFactory != null && resultFactory.Invoke(invocation) is TResult res)
+			{
+				base.SetResult(res);
+			}
+			return base.HandleAsync(invocation);
+		}
+	}
 }
