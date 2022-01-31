04baa4a07171a6447c9a0c7e563219fc56173ed5
Steven Giesel <stgiesel35@gmail.com>
2022-01-31 12:42:19 +0100

    feat: Added additional introduced flags in MouseEventArgs (net5 or higher) for Click event

--- src/bunit.web/EventDispatchExtensions/MouseEventDispatchExtensions.cs
+++ src/bunit.web/EventDispatchExtensions/MouseEventDispatchExtensions.cs
@@ -252,7 +252,76 @@ public static class MouseEventDispatchExtensions
 	/// <returns>A task that completes when the event handler is done.</returns>
 	public static Task MouseUpAsync(this IElement element, MouseEventArgs eventArgs) => element.TriggerEventAsync("onmouseup", eventArgs);
 
+#if NET6_0_OR_GREATER
 	/// <summary>
+	/// Raises the <c>@onclick</c> event on <paramref name="element"/>,  passing the provided
+	/// properties to the event handler via a <see cref="MouseEventArgs"/> object.
+	/// </summary>
+	/// <param name="element">The element to raise the event on.</param>
+	/// <param name="detail">A count of consecutive clicks that happened in a short amount of time, incremented by one.</param>
+	/// <param name="screenX">The X coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="screenY">The Y coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="clientX">The X coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="clientY">The Y coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="pageX">The X coordinate of the mouse pointer relative to the whole document.</param>
+	/// <param name="pageY">The Y coordinate of the mouse pointer relative to the whole document.</param>
+	/// <param name="offsetX">The X coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="offsetY">The Y coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="button">
+	///     The button number that was pressed when the mouse event was fired: Left button=0,
+	///     middle button=1 (if present), right button=2. For mice configured for left handed
+	///     use in which the button actions are reversed the values are instead read from
+	///     right to left.
+	/// </param>
+	/// <param name="buttons">
+	///     The buttons being pressed when the mouse event was fired: Left button=1, Right
+	///     button=2, Middle (wheel) button=4, 4th button (typically, "Browser Back" button)=8,
+	///     5th button (typically, "Browser Forward" button)=16. If two or more buttons are
+	///     pressed, returns the logical sum of the values. E.g., if Left button and Right
+	///     button are pressed, returns 3 (=1 | 2).
+	/// </param>
+	/// <param name="ctrlKey">true if the control key was down when the event was fired. false otherwise.</param>
+	/// <param name="shiftKey">true if the shift key was down when the event was fired. false otherwise.</param>
+	/// <param name="altKey">true if the alt key was down when the event was fired. false otherwise.</param>
+	/// <param name="metaKey">true if the meta key was down when the event was fired. false otherwise.</param>
+	/// <param name="type">Gets or sets the type of the event.</param>
+	public static void Click(this IElement element, long detail = 1, double screenX = default, double screenY = default, double clientX = default, double clientY = default, double pageX = 0, double pageY = 0, double offsetX = 0, double offsetY = 0, long button = default, long buttons = default, bool ctrlKey = default, bool shiftKey = default, bool altKey = default, bool metaKey = default, string? type = default)
+		=> _ = ClickAsync(element, new MouseEventArgs { Detail = detail, ScreenX = screenX, ScreenY = screenY, ClientX = clientX, PageX = pageX, PageY = pageY, OffsetX = offsetX, OffsetY = offsetY, ClientY = clientY, Button = button, Buttons = buttons, CtrlKey = ctrlKey, ShiftKey = shiftKey, AltKey = altKey, MetaKey = metaKey, Type = type! });
+#elif NET5_0
+	/// <summary>
+	/// Raises the <c>@onclick</c> event on <paramref name="element"/>,  passing the provided
+	/// properties to the event handler via a <see cref="MouseEventArgs"/> object.
+	/// </summary>
+	/// <param name="element">The element to raise the event on.</param>
+	/// <param name="detail">A count of consecutive clicks that happened in a short amount of time, incremented by one.</param>
+	/// <param name="screenX">The X coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="screenY">The Y coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="clientX">The X coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="clientY">The Y coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="offsetX">The X coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="offsetY">The Y coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="button">
+	///     The button number that was pressed when the mouse event was fired: Left button=0,
+	///     middle button=1 (if present), right button=2. For mice configured for left handed
+	///     use in which the button actions are reversed the values are instead read from
+	///     right to left.
+	/// </param>
+	/// <param name="buttons">
+	///     The buttons being pressed when the mouse event was fired: Left button=1, Right
+	///     button=2, Middle (wheel) button=4, 4th button (typically, "Browser Back" button)=8,
+	///     5th button (typically, "Browser Forward" button)=16. If two or more buttons are
+	///     pressed, returns the logical sum of the values. E.g., if Left button and Right
+	///     button are pressed, returns 3 (=1 | 2).
+	/// </param>
+	/// <param name="ctrlKey">true if the control key was down when the event was fired. false otherwise.</param>
+	/// <param name="shiftKey">true if the shift key was down when the event was fired. false otherwise.</param>
+	/// <param name="altKey">true if the alt key was down when the event was fired. false otherwise.</param>
+	/// <param name="metaKey">true if the meta key was down when the event was fired. false otherwise.</param>
+	/// <param name="type">Gets or sets the type of the event.</param>
+	public static void Click(this IElement element, long detail = 1, double screenX = default, double screenY = default, double clientX = default, double clientY = default, double offsetX = 0, double offsetY = 0, long button = default, long buttons = default, bool ctrlKey = default, bool shiftKey = default, bool altKey = default, bool metaKey = default, string? type = default)
+		=> _ = ClickAsync(element, new MouseEventArgs { Detail = detail, ScreenX = screenX, ScreenY = screenY, ClientX = clientX, OffsetX = offsetX, OffsetY = offsetY, ClientY = clientY, Button = button, Buttons = buttons, CtrlKey = ctrlKey, ShiftKey = shiftKey, AltKey = altKey, MetaKey = metaKey, Type = type! });
+#else
+		/// <summary>
 	/// Raises the <c>@onclick</c> event on <paramref name="element"/>,  passing the provided
 	/// properties to the event handler via a <see cref="MouseEventArgs"/> object.
 	/// </summary>
@@ -282,6 +351,8 @@ public static class MouseEventDispatchExtensions
 	/// <param name="type">Gets or sets the type of the event.</param>
 	public static void Click(this IElement element, long detail = 1, double screenX = default, double screenY = default, double clientX = default, double clientY = default, long button = default, long buttons = default, bool ctrlKey = default, bool shiftKey = default, bool altKey = default, bool metaKey = default, string? type = default)
 		=> _ = ClickAsync(element, new MouseEventArgs { Detail = detail, ScreenX = screenX, ScreenY = screenY, ClientX = clientX, ClientY = clientY, Button = button, Buttons = buttons, CtrlKey = ctrlKey, ShiftKey = shiftKey, AltKey = altKey, MetaKey = metaKey, Type = type! });
+#endif
+
 
 	/// <summary>
 	/// Raises the <c>@onclick</c> event on <paramref name="element"/>, passing the provided <paramref name="eventArgs"/>
@@ -300,6 +371,76 @@ public static class MouseEventDispatchExtensions
 	/// <param name="eventArgs">The event arguments to pass to the event handler.</param>
 	/// <returns>A task that completes when the event handler is done.</returns>
 	public static Task ClickAsync(this IElement element, MouseEventArgs eventArgs) => element.TriggerEventAsync("onclick", eventArgs);
+
+#if NET6_0_OR_GREATER
+	/// <summary>
+	/// Raises the <c>@ondblclick</c> event on <paramref name="element"/>,  passing the provided
+	/// properties to the event handler via a <see cref="MouseEventArgs"/> object.
+	/// </summary>
+	/// <param name="element">The element to raise the event on.</param>
+	/// <param name="detail">A count of consecutive clicks that happened in a short amount of time, incremented by one.</param>
+	/// <param name="screenX">The X coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="screenY">The Y coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="clientX">The X coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="clientY">The Y coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="pageX">The X coordinate of the mouse pointer relative to the whole document.</param>
+	/// <param name="pageY">The Y coordinate of the mouse pointer relative to the whole document.</param>
+	/// <param name="offsetX">The X coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="offsetY">The Y coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="button">
+	///     The button number that was pressed when the mouse event was fired: Left button=0,
+	///     middle button=1 (if present), right button=2. For mice configured for left handed
+	///     use in which the button actions are reversed the values are instead read from
+	///     right to left.
+	/// </param>
+	/// <param name="buttons">
+	///     The buttons being pressed when the mouse event was fired: Left button=1, Right
+	///     button=2, Middle (wheel) button=4, 4th button (typically, "Browser Back" button)=8,
+	///     5th button (typically, "Browser Forward" button)=16. If two or more buttons are
+	///     pressed, returns the logical sum of the values. E.g., if Left button and Right
+	///     button are pressed, returns 3 (=1 | 2).
+	/// </param>
+	/// <param name="ctrlKey">true if the control key was down when the event was fired. false otherwise.</param>
+	/// <param name="shiftKey">true if the shift key was down when the event was fired. false otherwise.</param>
+	/// <param name="altKey">true if the alt key was down when the event was fired. false otherwise.</param>
+	/// <param name="metaKey">true if the meta key was down when the event was fired. false otherwise.</param>
+	/// <param name="type">Gets or sets the type of the event.</param>
+	public static void DoubleClick(this IElement element, long detail = 2, double screenX = default, double screenY = default, double clientX = default, double clientY = default, double pageX = 0, double pageY = 0, double offsetX = 0, double offsetY = 0, long button = default, long buttons = default, bool ctrlKey = default, bool shiftKey = default, bool altKey = default, bool metaKey = default, string? type = default)
+		=> _ = DoubleClickAsync(element, new MouseEventArgs { Detail = detail, ScreenX = screenX, ScreenY = screenY, ClientX = clientX, ClientY = clientY, PageX = pageX, PageY = pageY, OffsetX = offsetX, OffsetY = offsetY, Button = button, Buttons = buttons, CtrlKey = ctrlKey, ShiftKey = shiftKey, AltKey = altKey, MetaKey = metaKey, Type = type! });
+#elif NET5_0
+	/// <summary>
+	/// Raises the <c>@ondblclick</c> event on <paramref name="element"/>,  passing the provided
+	/// properties to the event handler via a <see cref="MouseEventArgs"/> object.
+	/// </summary>
+	/// <param name="element">The element to raise the event on.</param>
+	/// <param name="detail">A count of consecutive clicks that happened in a short amount of time, incremented by one.</param>
+	/// <param name="screenX">The X coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="screenY">The Y coordinate of the mouse pointer in global (screen) coordinates.</param>
+	/// <param name="clientX">The X coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="clientY">The Y coordinate of the mouse pointer in local (DOM content) coordinates.</param>
+	/// <param name="offsetX">The X coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="offsetY">The Y coordinate of the mouse pointer in relative (Target Element) coordinates.</param>
+	/// <param name="button">
+	///     The button number that was pressed when the mouse event was fired: Left button=0,
+	///     middle button=1 (if present), right button=2. For mice configured for left handed
+	///     use in which the button actions are reversed the values are instead read from
+	///     right to left.
+	/// </param>
+	/// <param name="buttons">
+	///     The buttons being pressed when the mouse event was fired: Left button=1, Right
+	///     button=2, Middle (wheel) button=4, 4th button (typically, "Browser Back" button)=8,
+	///     5th button (typically, "Browser Forward" button)=16. If two or more buttons are
+	///     pressed, returns the logical sum of the values. E.g., if Left button and Right
+	///     button are pressed, returns 3 (=1 | 2).
+	/// </param>
+	/// <param name="ctrlKey">true if the control key was down when the event was fired. false otherwise.</param>
+	/// <param name="shiftKey">true if the shift key was down when the event was fired. false otherwise.</param>
+	/// <param name="altKey">true if the alt key was down when the event was fired. false otherwise.</param>
+	/// <param name="metaKey">true if the meta key was down when the event was fired. false otherwise.</param>
+	/// <param name="type">Gets or sets the type of the event.</param>
+	public static void DoubleClick(this IElement element, long detail = 2, double screenX = default, double screenY = default, double clientX = default, double clientY = default, double offsetX = 0, double offsetY = 0, long button = default, long buttons = default, bool ctrlKey = default, bool shiftKey = default, bool altKey = default, bool metaKey = default, string? type = default)
+		=> _ = DoubleClickAsync(element, new MouseEventArgs { Detail = detail, ScreenX = screenX, ScreenY = screenY, ClientX = clientX, ClientY = clientY, OffsetX = offsetX, OffsetY = offsetY, Button = button, Buttons = buttons, CtrlKey = ctrlKey, ShiftKey = shiftKey, AltKey = altKey, MetaKey = metaKey, Type = type! });
+#else
 
 	/// <summary>
 	/// Raises the <c>@ondblclick</c> event on <paramref name="element"/>,  passing the provided
@@ -331,7 +472,7 @@ public static class MouseEventDispatchExtensions
 	/// <param name="type">Gets or sets the type of the event.</param>
 	public static void DoubleClick(this IElement element, long detail = 2, double screenX = default, double screenY = default, double clientX = default, double clientY = default, long button = default, long buttons = default, bool ctrlKey = default, bool shiftKey = default, bool altKey = default, bool metaKey = default, string? type = default)
 		=> _ = DoubleClickAsync(element, new MouseEventArgs { Detail = detail, ScreenX = screenX, ScreenY = screenY, ClientX = clientX, ClientY = clientY, Button = button, Buttons = buttons, CtrlKey = ctrlKey, ShiftKey = shiftKey, AltKey = altKey, MetaKey = metaKey, Type = type! });
-
+#endif
 	/// <summary>
 	/// Raises the <c>@ondblclick</c> event on <paramref name="element"/>, passing the provided <paramref name="eventArgs"/>
 	/// to the event handler.
