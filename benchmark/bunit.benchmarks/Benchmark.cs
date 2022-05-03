6279c2d857f7531835e3c6b9180bbb230cf1e50b
Steven Giesel <stgiesel35@gmail.com>
2022-05-03 18:26:03 +0200

    add: Setup for benchmarking

---
+++ benchmark/bunit.benchmarks/Benchmark.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using bunit.benchmarks.assets;
+
+namespace Bunit;
+
+[MemoryDiagnoser]
+public class Benchmark : TestContext
+{
+    [Benchmark]
+    public IRenderedComponent<Counter> RenderCounter()
+    {
+        return RenderComponent<Counter>();
+    }
+}
