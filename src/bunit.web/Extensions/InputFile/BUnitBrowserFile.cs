8c3ae90574c02d3816837c6b9efe3c6f09a62fbc
Steven Giesel <stgiesel35@gmail.com>
2022-05-09 12:36:36 +0200

    feat: Added InputFile setup (#700)
    * feat: Added InputFile setup
    
    Co-authored-by: Egil Hansen <egil@assimilated.dk>

---
+++ src/bunit.web/Extensions/InputFile/BUnitBrowserFile.cs
@@ -0,0 +1,33 @@
+﻿#if NET5_0_OR_GREATER
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Bunit;
+
+internal class BUnitBrowserFile : IBrowserFile
+{
+    public string Name { get; }
+    public DateTimeOffset LastModified { get; }
+    public long Size { get; }
+    public string ContentType { get; }
+    public byte[] Content { get; }
+
+    public BUnitBrowserFile(
+        string name,
+        DateTimeOffset lastModified,
+        long size,
+        string contentType,
+        byte[] content)
+    {
+        Name = name;
+        LastModified = lastModified;
+        Size = size;
+        ContentType = contentType;
+        Content = content;
+    }
+
+    public Stream OpenReadStream(long maxAllowedSize = 512000, CancellationToken cancellationToken = default)
+    {
+        return new MemoryStream(Content);
+    }
+}
+#endif
