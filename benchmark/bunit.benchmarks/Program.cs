6296315fca13ee304efc01af69fc5fccb972292c
Steven Giesel <stgiesel35@gmail.com>
2022-05-11 18:47:42 +0200

    Setup for benchmarking (#704)
    * add: Setup for benchmarking

---
+++ benchmark/bunit.benchmarks/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using Bunit;
+
+BenchmarkSwitcher.FromAssembly(typeof(Benchmark).Assembly).RunAll();
