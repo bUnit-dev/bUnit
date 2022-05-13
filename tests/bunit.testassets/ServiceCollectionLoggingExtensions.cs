40e7e065e01e3bd99b1844d5a32eb907abfec6b7
Egil Hansen <egil@assimilated.dk>
2022-05-13 15:44:53 +0000

    feat: add thread id to logging in test output

--- tests/bunit.testassets/ServiceCollectionLoggingExtensions.cs
+++ tests/bunit.testassets/ServiceCollectionLoggingExtensions.cs
@@ -1,5 +1,7 @@
+using System.Globalization;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Core;
 using Serilog.Events;
 using Xunit.Abstractions;
 
@@ -12,11 +14,26 @@ public static class ServiceCollectionLoggingExtensions
 	{
 		var serilogLogger = new LoggerConfiguration()
 			.MinimumLevel.Verbose()
-			.WriteTo.TestOutput(outputHelper, LogEventLevel.Verbose)
+			.Enrich.With(new ThreadIDEnricher())
+			.WriteTo.TestOutput(
+				testOutputHelper: outputHelper,
+				restrictedToMinimumLevel: LogEventLevel.Verbose,
+				outputTemplate: ThreadIDEnricher.DefaultConsoleOutputTemplate)
 			.CreateLogger();
 
 		services.AddSingleton<ILoggerFactory>(new LoggerFactory().AddSerilog(serilogLogger, dispose: true));
 		services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
 		return services;
+	}
+
+	private class ThreadIDEnricher : ILogEventEnricher
+	{
+		internal const string DefaultConsoleOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} ({ThreadID}) [{Level}] {Message}{NewLine}{Exception}";
+
+		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+		{
+			logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+			  "ThreadID", Environment.CurrentManagedThreadId.ToString("D4", CultureInfo.InvariantCulture)));
+		}
 	}
 }
