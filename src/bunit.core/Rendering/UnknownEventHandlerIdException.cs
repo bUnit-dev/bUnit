811b32baa9e508ae9e1a5edce8114ec557e20e6e
Egil Hansen <egil@assimilated.dk>
2022-05-25 23:50:26 +0000

    fix: improve exception message in UnknownEventHandlerIdException

--- src/bunit.core/Rendering/UnknownEventHandlerIdException.cs
+++ src/bunit.core/Rendering/UnknownEventHandlerIdException.cs
@@ -20,7 +20,12 @@ public sealed class UnknownEventHandlerIdException : Exception
 
 	private static string CreateMessage(ulong eventHandlerId, EventFieldInfo fieldInfo)
 		=> $"There is no event handler with ID '{eventHandlerId}' associated with the '{fieldInfo.FieldValue}' event " +
-			"in the current render tree. This can happen, for example, when using cut.FindAll(), and calling event trigger methods " +
-			"on the found elements after a re-render of the render tree. The workaround is to use re-issue the cut.FindAll() after " +
-			"each render of a component, this ensures you have the latest version of the render tree and DOM tree available in your test code.";
+		   $"in the current render tree.{Environment.NewLine}{Environment.NewLine}" +
+		   $"This can happen, for example, when using `cut.FindAll()`, and calling event trigger methods " +
+			"on the found elements after a re-render of the render tree. The workaround is to use re-issue the `cut.FindAll()` call after " +
+           $"each render of a component, this ensures you have the latest version of the render tree and DOM tree available in your test code.{Environment.NewLine}{Environment.NewLine}" +
+		   $"This can happen with code like this, `cut.Find(\"button\").Click()`, where the buttons event handler can be removed " +
+		   $"between the time button is found with the Find method and the Click method is triggered. The workaround is to use " +
+		   $"wrap the Find and Click method calls in InvokeAsync, i.e. `cut.InvokeAsync(() => cut.Find(\"button\").Click());`. " +
+		   $"This ensures that there are no changes to the DOM between Find method and the Click method calls.";
 }
