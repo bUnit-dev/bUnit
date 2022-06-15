a7527c99fc7bc26d3e7476f4752aa64e329aee23
Egil Hansen <egil@assimilated.dk>
2022-06-15 16:36:55 +0000

    chore: remove support for .netstandard2.1 and net5.0

--- tests/bunit.web.tests/JSInterop/JSRuntimeUnhandledInvocationExceptionTest.cs
+++ tests/bunit.web.tests/JSInterop/JSRuntimeUnhandledInvocationExceptionTest.cs
@@ -307,6 +307,22 @@ public class JSRuntimeUnhandledInvocationExceptionTest
 		Assert.Equal(exectedErrorMessage, sut.Message);
 	}
 
+	[Theory(DisplayName = "Message prints correctly when trying to import an unconfigured module")]
+	[AutoData]
+	public void Test036(string moduleName)
+	{
+		var identifier = "import";
+		var returnType = typeof(IJSObjectReference);
+		var invocationMethodName = "InvokeAsync";
+		var exectedErrorMessage = CreateExpectedErrorMessage(
+			$"{CodeIdent}{invocationMethodName}<{returnType.Name}>(\"{identifier}\", \"{moduleName}\")",
+			$"{CodeIdent}SetupModule(\"{moduleName}\")");
+
+		var sut = new JSRuntimeUnhandledInvocationException(new JSRuntimeInvocation(identifier, new object?[] { moduleName }, returnType, invocationMethodName));
+
+		Assert.Equal(exectedErrorMessage, sut.Message);
+	}
+
 	public class Dummy1 { }
 	public class Dummy2 { }
 	public class Dummy3 { }
