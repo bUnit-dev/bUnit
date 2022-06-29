799820b4bb6a7b7ca36d1490131e67aa39b02923
Simon Cropp <simon.cropp@gmail.com>
2022-06-29 19:40:30 +1000

    Update ParameterException.cs

--- src/bunit.core/ParameterException.cs
+++ src/bunit.core/ParameterException.cs
@@ -9,7 +9,7 @@ public sealed class ParameterException : ArgumentException
 	/// <summary>
 	/// Initializes a new instance of the <see cref="ParameterException"/> class.
 	/// </summary>
-	/// <param name="messsage">Validation message.</param>
+	/// <param name="message">Validation message.</param>
 	/// <param name="parameterName">Name of the Blazor parameter.</param>
 	public ParameterException(string message, string parameterName)
 		: base(message, parameterName)
