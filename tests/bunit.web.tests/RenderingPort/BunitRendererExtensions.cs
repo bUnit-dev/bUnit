4904ff34f932a3b6126a11ab47f9a8b89dae9230
Egil Hansen <egil@assimilated.dk>
2022-07-04 13:39:12 +0000

    wip: partial port of Renderer (BunitRenderer)

---
+++ tests/bunit.web.tests/RenderingPort/BunitRendererExtensions.cs
@@ -0,0 +1,24 @@
+namespace Bunit.RenderingPort;
+
+internal static class BunitRendererExtensions
+{
+	public static IRenderedComponent<IComponent> Render<TComponent>(this BunitRenderer renderer)
+		where TComponent : IComponent
+		=> renderer.Render(builder =>
+		{
+			builder.OpenComponent<TComponent>(0);
+			builder.CloseComponent();
+		});
+
+	public static async Task<IRenderedComponent<IComponent>> RenderAsync<TComponent>(this BunitRenderer renderer)
+		where TComponent : IComponent
+	{
+		var rc = await renderer.RenderAsync(builder =>
+			{
+				builder.OpenComponent<TComponent>(0);
+				builder.CloseComponent();
+			}).ConfigureAwait(false);
+
+		return rc;
+	}
+}
