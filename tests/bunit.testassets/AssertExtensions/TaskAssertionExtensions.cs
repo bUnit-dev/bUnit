ca42b143ce42f928200883053c3eea97ca021b9d
Egil Hansen <egil@assimilated.dk>
2022-12-04 22:54:49 +0000

    refactor: use .net6 wait timeout feature instead of home grown

--- tests/bunit.testassets/AssertExtensions/TaskAssertionExtensions.cs
+++ tests/bunit.testassets/AssertExtensions/TaskAssertionExtensions.cs
@@ -2,17 +2,8 @@ namespace Bunit;
 
 public static class TaskAssertionExtensions
 {
-	public static async Task ShouldCompleteWithin(this Task task, TimeSpan timeout)
+	public static Task ShouldCompleteWithin(this Task task, TimeSpan timeout)
 	{
-#if NET6_0_OR_GREATER
-		await task.WaitAsync(timeout);
-#else
-		using var cts = new CancellationTokenSource();
-        var delayTask = Task.Delay(timeout, cts.Token);
-        if (task != await Task.WhenAny(task, delayTask))
-            throw new TimeoutException();
-
-        cts.Cancel();
-#endif
+		return task.WaitAsync(timeout);
 	}
 }
