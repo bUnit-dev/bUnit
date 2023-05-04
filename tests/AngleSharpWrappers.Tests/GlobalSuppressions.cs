cbf5ad97b4b771d54063f3774f9ee5d28ced8352
Steven Giesel <stgiesel35@gmail.com>
2023-05-04 22:04:08 +0200

    remove anglesharpwrappers entirely

--- tests/AngleSharpWrappers.Tests/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1013:Public method should be marked as test", Justification = "<Pending>")]
