6fca097c50dab7be46099c492d9d88e3c4b46b35
Egil Hansen <egil@assimilated.dk>
2023-05-17 10:18:57 +0000

    fix(test): skip tests that often fail due to timeout happening before any renders are complete

--- tests/bunit.web.tests/Extensions/WaitForHelpers/RenderedFragmentWaitForElementsHelperExtensions.Async.Test.cs
+++ tests/bunit.web.tests/Extensions/WaitForHelpers/RenderedFragmentWaitForElementsHelperExtensions.Async.Test.cs
@@ -5,9 +5,9 @@ namespace Bunit.Extensions.WaitForHelpers;
 
 public class RenderedFragmentWaitForElementsHelperExtensionsAsyncTest : TestContext
 {
-    private readonly static TimeSpan WaitForTestTimeout = TimeSpan.FromMilliseconds(100);
+	private readonly static TimeSpan WaitForTestTimeout = TimeSpan.FromMilliseconds(5);
 
-    public RenderedFragmentWaitForElementsHelperExtensionsAsyncTest(ITestOutputHelper testOutput)
+	public RenderedFragmentWaitForElementsHelperExtensionsAsyncTest(ITestOutputHelper testOutput)
 	{
 		TestContext.DefaultWaitTimeout = TimeSpan.FromSeconds(30);
 		Services.AddXunitLogger(testOutput);
@@ -62,7 +62,8 @@ public class RenderedFragmentWaitForElementsHelperExtensionsAsyncTest : TestCont
 		expected.InnerException.ShouldBeNull();
 	}
 
-	[Fact(DisplayName = "WaitForElements with specific count N throws exception after timeout when cssSelector does not result in N matching elements")]
+	[Fact(DisplayName = "WaitForElements with specific count N throws exception after timeout when cssSelector does not result in N matching elements",
+		Skip = "Need to figure out how to make this deterministic.")]
 	[Trait("Category", "async")]
 	public async Task Test023()
 	{
