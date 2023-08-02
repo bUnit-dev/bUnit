82f33f20ac6bbef341b3ae6a107b604dc268d6bf
Steven Giesel <stgiesel35@gmail.com>
2023-08-02 21:33:21 +0200

    refactor: Use record instead of implementing ourselves

--- src/bunit/JSInterop/JSRuntimeInvocation.cs
+++ src/bunit/JSInterop/JSRuntimeInvocation.cs
@@ -3,7 +3,7 @@ namespace Bunit;
 /// <summary>
 /// Represents an invocation of JavaScript via the JSRuntime Mock.
 /// </summary>
-public readonly struct JSRuntimeInvocation : IEquatable<JSRuntimeInvocation>
+public readonly record struct JSRuntimeInvocation
 {
 	/// <summary>
 	/// Gets the identifier used in the invocation.
@@ -66,70 +66,5 @@ public readonly struct JSRuntimeInvocation : IEquatable<JSRuntimeInvocation>
 		ResultType = resultType;
 		InvocationMethodName = invocationMethodName;
 		IsVoidResultInvocation = resultType == typeof(Microsoft.JSInterop.Infrastructure.IJSVoidResult);
-	}
-
-	/// <inheritdoc/>
-	public bool Equals(JSRuntimeInvocation other)
-		=> Identifier.Equals(other.Identifier, StringComparison.Ordinal)
-		&& CancellationToken == other.CancellationToken
-		&& ArgumentsEqual(Arguments, other.Arguments)
-		&& ResultType == other.ResultType
-		&& InvocationMethodName.Equals(other.InvocationMethodName, StringComparison.Ordinal);
-
-	/// <inheritdoc/>
-	public override bool Equals(object? obj) => obj is JSRuntimeInvocation other && Equals(other);
-
-	/// <inheritdoc/>
-	public override int GetHashCode()
-	{
-		var hash = default(HashCode);
-		hash.Add(Identifier);
-		hash.Add(CancellationToken);
-		hash.Add(ResultType);
-		hash.Add(InvocationMethodName);
-
-		for (var i = 0; i < Arguments.Count; i++)
-		{
-			hash.Add(Arguments[i]);
-		}
-
-		return hash.ToHashCode();
-	}
-
-	/// <summary>
-	/// Verify whether <paramref name="left"/> and <paramref name="right"/>
-	/// <see cref="JSRuntimeInvocation"/> is equal.
-	/// </summary>
-	public static bool operator ==(JSRuntimeInvocation left, JSRuntimeInvocation right) => left.Equals(right);
-
-	/// <summary>
-	/// Verify whether <paramref name="left"/> and <paramref name="right"/>
-	/// <see cref="JSRuntimeInvocation"/> is not equal.
-	/// </summary>
-	public static bool operator !=(JSRuntimeInvocation left, JSRuntimeInvocation right) => !(left == right);
-
-	private static bool ArgumentsEqual(IReadOnlyList<object?> left, IReadOnlyList<object?> right)
-	{
-		if (left.Count != right.Count)
-			return false;
-
-		for (var i = 0; i < left.Count; i++)
-		{
-			var l = left[i];
-			var r = right[i];
-
-			if (l is null)
-			{
-				if (r is object)
-					return false;
-			}
-			else
-			{
-				if (!l.Equals(right[i]))
-					return false;
-			}
-		}
-
-		return true;
 	}
 }
