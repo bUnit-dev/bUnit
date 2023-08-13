c8202c414f55a4070b109654f00ce462297a8302
Steven Giesel <stgiesel35@gmail.com>
2023-08-13 21:12:21 +0200

    feat: Increase timeout to 5 minutes if a debugger is attached (#1178)
    * feat: Increase timeout to Infinite if a debugger is attached
    
    * Added docs
    
    * Update docs/site/docs/interaction/awaiting-async-state.md
    
    Co-authored-by: Egil Hansen <egil@assimilated.dk>
    
    * Update docs/site/docs/interaction/awaiting-async-state.md
    
    Co-authored-by: Egil Hansen <egil@assimilated.dk>
    
    * Always remove the timeout when debugger is attached
    
    * Update docs
    
    ---------
    
    Co-authored-by: Egil Hansen <egil@assimilated.dk>

--- src/bunit.core/Extensions/WaitForHelpers/RenderedFragmentWaitForHelperExtensions.cs
+++ src/bunit.core/Extensions/WaitForHelpers/RenderedFragmentWaitForHelperExtensions.cs
@@ -20,6 +20,9 @@ public static class RenderedFragmentWaitForHelperExtensions
 	/// <param name="statePredicate">The predicate to invoke after each render, which must returns <c>true</c> when the desired state has been reached.</param>
 	/// <param name="timeout">The maximum time to wait for the desired state.</param>
 	/// <exception cref="WaitForFailedException">Thrown if the <paramref name="statePredicate"/> throw an exception during invocation, or if the timeout has been reached. See the inner exception for details.</exception>
+	/// <remarks>
+	/// If a debugger is attached the timeout is set to <see cref="Timeout.InfiniteTimeSpan" />, giving the possibility to debug without the timeout triggering.
+	/// </remarks>
 	public static void WaitForState(this IRenderedFragmentBase renderedFragment, Func<bool> statePredicate, TimeSpan? timeout = null)
 	{
 		using var waiter = new WaitForStateHelper(renderedFragment, statePredicate, timeout);
