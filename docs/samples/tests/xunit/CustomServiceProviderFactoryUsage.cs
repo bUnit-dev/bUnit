43fe591579f9e2a477ce030787bcecae3340873a
Alexander Bülow <alexander.buelow@gmail.com>
2023-10-12 09:58:41 +0200

    Allow custom service providers like autofac Documentation

--- docs/samples/tests/xunit/CustomServiceProviderFactoryUsage.cs
+++ docs/samples/tests/xunit/CustomServiceProviderFactoryUsage.cs
@@ -1,3 +1,5 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -24,4 +26,57 @@ public class CustomServiceProviderFactoryUsage : TestContext {
     Assert.NotNull(dummyService);
   }
 
+  [Fact]
+  public void AutofacServiceProviderViaFactoryReturns() {
+    void ConfigureContainer(ContainerBuilder containerBuilder) {
+      containerBuilder
+        .RegisterType<DummyService>()
+        .AsSelf();
+    }
+
+    Services.UseServiceProviderFactory(new AutofacServiceProviderFactory(ConfigureContainer));
+
+    //get a service which was installed in the Autofac ContainerBuilder
+
+    var dummyService = Services.GetService<DummyService>();
+
+    Assert.NotNull(dummyService);
+
+    //get a service which was installed in the bUnit ServiceCollection
+
+    var testContextBase = Services.GetService<TestContextBase>();
+
+    Assert.NotNull(testContextBase);
+    Assert.Equal(this, testContextBase);
+  }
+
+  [Fact]
+  public void AutofacServiceProviderViaDelegateReturns() {
+    ILifetimeScope ConfigureContainer(IServiceCollection services) {
+      var containerBuilder = new ContainerBuilder();
+
+      containerBuilder
+        .RegisterType<DummyService>()
+        .AsSelf();
+
+      containerBuilder.Populate(services);
+
+      return containerBuilder.Build();
+    }
+
+    Services.UseServiceProviderFactory(x => new AutofacServiceProvider(ConfigureContainer(x)));
+
+    //get a service which was installed in the Autofac ContainerBuilder
+
+    var dummyService = Services.GetService<DummyService>();
+
+    Assert.NotNull(dummyService);
+
+    //get a service which was installed in the bUnit ServiceCollection
+
+    var testContextBase = Services.GetService<TestContextBase>();
+
+    Assert.NotNull(testContextBase);
+    Assert.Equal(this, testContextBase);
+  }
 }
