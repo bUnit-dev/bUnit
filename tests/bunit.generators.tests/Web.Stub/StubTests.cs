134723c6ba1b631fea0c6150f445669d314837dd
Steven Giesel <stgiesel35@gmail.com>
2023-11-25 16:36:03 +0100

    feat: Add simple test

--- tests/bunit.generators.tests/Web.Stub/StubTests.cs
+++ tests/bunit.generators.tests/Web.Stub/StubTests.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Components;
 
 namespace Bunit.Web.Stub;
@@ -5,11 +6,28 @@ namespace Bunit.Web.Stub;
 public class StubTests
 {
 	[Fact]
-	public void Test001()
-	{}
+	public void Stubbed_component_has_same_parameters()
+	{
+		var counterComponentStubProperties = typeof(CounterComponentStub).GetProperties();
+
+		foreach (var prop in typeof(CounterComponent).GetProperties())
+		{
+			var matchingProp = counterComponentStubProperties.FirstOrDefault(p => p.Name == prop.Name);
+
+			Assert.NotNull(matchingProp);
+
+			var isParameter = prop.GetCustomAttribute(typeof(ParameterAttribute)) is not null;
+			var stubIsParameter = matchingProp.GetCustomAttribute(typeof(ParameterAttribute)) is not  null;
+			Assert.Equal(isParameter, stubIsParameter);
+
+			var isCascadingParameter = prop.GetCustomAttribute(typeof(CascadingParameterAttribute)) is not  null;
+			var stubIsCascadingParameter = matchingProp.GetCustomAttribute(typeof(CascadingParameterAttribute)) is not  null;
+			Assert.Equal(isCascadingParameter, stubIsCascadingParameter);
+		}
+	}
 	
 	[Stub(typeof(CounterComponentStub))]
-	public class CounterComponentStub : ComponentBase
+	public partial class CounterComponentStub : ComponentBase
 	{
 	}
 }
