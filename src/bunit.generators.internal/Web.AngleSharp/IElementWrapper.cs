b8f671fe94e4c491b3cb05207b6aa5ff2365256b
Steven Giesel <stgiesel35@gmail.com>
2023-11-25 15:41:49 +0100

    feat: Create stub generator in public project

---
+++ src/bunit.generators.internal/Web.AngleSharp/IElementWrapper.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using AngleSharp.Dom;
+using System.CodeDom.Compiler;
+
+namespace Bunit.Web.AngleSharp;
+
+/// <summary>
+/// Represents a wrapper around an <typeparamref name="TElement"/>.
+/// </summary>
+[GeneratedCodeAttribute("Bunit.Web.AngleSharp", "1.0.0.0")]
+internal interface IElementWrapper<out TElement> where TElement : class, IElement
+{
+	/// <summary>
+	/// Gets the wrapped element.
+	/// </summary>
+	TElement WrappedElement { get; }
+}
+#nullable restore
