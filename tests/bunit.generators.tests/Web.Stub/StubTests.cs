6828160145aa28e034ec1748c2cdaba4ad68cb10
Steven Giesel <stgiesel35@gmail.com>
2023-11-26 21:53:12 +0100

    feat: Add more tests

--- tests/bunit.generators.tests/Web.Stub/StubTests.cs
+++ tests/bunit.generators.tests/Web.Stub/StubTests.cs
@@ -12,4 +12,14 @@ public class StubTests : TestContext
 		var child = cut.FindComponent<CounterComponentStub>();
 		Assert.Equal(2, child.Instance.Count);
 	}
+
+	[Fact]
+	public void Stubbed_component_can_be_used_in_multiple_tests()
+	{
+		ComponentFactories.AddGeneratedStub<CounterComponent>();
+
+		var cut = RenderComponent<ParentComponent>();
+		
+		Assert.True(cut.HasComponent<CounterComponentStub>());
+	}
 }
