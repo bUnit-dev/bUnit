6d7285a42200249e1f8d682889d10dc215e4cb48
Steven Giesel <stgiesel35@gmail.com>
2023-11-26 21:53:12 +0100

    feat: Add more tests

--- src/bunit.generators/Web.Stubs/StubGenerator.cs
+++ src/bunit.generators/Web.Stubs/StubGenerator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
