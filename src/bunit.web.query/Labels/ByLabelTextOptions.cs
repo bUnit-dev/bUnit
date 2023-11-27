7ebab187ad8a015dcc3f20337039dfd7ec39f323
scottsauber <stsauber@gmail.com>
2023-11-27 00:05:38 -0600

    chore: add xml docs to indicate defaults of ByLabelTextOptions

--- src/bunit.web.query/Labels/ByLabelTextOptions.cs
+++ src/bunit.web.query/Labels/ByLabelTextOptions.cs
@@ -1,7 +1,17 @@
 namespace Bunit;
 
+/// <summary>
+/// Allows overrides of behavior for FindByLabelText method
+/// </summary>
 public record ByLabelTextOptions
 {
+	/// <summary>
+	/// The default behavior used by FindByLabelText if no overrides are specified
+	/// </summary>
 	public static readonly ByLabelTextOptions Default = new();
+
+	/// <summary>
+	/// The StringComparison used for comparing the desired Label Text to the resulting HTML. Defaults to Ordinal (case sensitive).
+	/// </summary>
 	public StringComparison ComparisonType { get; set; } = StringComparison.Ordinal;
 }
