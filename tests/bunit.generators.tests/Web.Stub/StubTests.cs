bb982f1145c6dfe6879235f6c17984ae3f349a62
Steven Giesel <stgiesel35@gmail.com>
2023-11-28 07:32:16 +0100

    Use render tests instead of reflection

--- tests/bunit.generators.tests/Web.Stub/StubTests.cs
+++ tests/bunit.generators.tests/Web.Stub/StubTests.cs
@@ -18,17 +18,33 @@ public class StubTests : TestContext
 	}
 
 	[Fact]
-	public void Stubbed_component_has_same_parameter_values()
+	public void Generated_stub_can_handle_cascading_parameters()
 	{
-		var type = typeof(CounterComponentStub);
-		var cascadingParameterValue = type.GetProperty(nameof(CounterComponentStub.CascadingCount))
-			?.GetCustomAttribute<CascadingParameterAttribute>()
-			?.Name;
-		var captureUnmatchedValues = type.GetProperty(nameof(CounterComponentStub.UnmatchedValues))
-			?.GetCustomAttribute<ParameterAttribute>()
-			?.CaptureUnmatchedValues;
-		
-		Assert.Equal("Cascading", cascadingParameterValue);
-		Assert.True(captureUnmatchedValues);
+		ComponentFactories.AddGeneratedStub<CounterComponent>();
+
+		var cut = RenderComponent<ParentComponent>(p => p.AddCascadingValue("Cascading", 3));
+
+		var stub = cut.FindComponent<CounterComponentStub>();
+		Assert.Equal(3, stub.Instance.CascadingCount);
+	}
+
+	[Fact]
+	public void Generated_stub_can_handle_unmatched_parameters()
+	{
+		ComponentFactories.AddGeneratedStub<CounterComponent>();
+
+		var cut = Render(builder =>
+		{
+			builder.OpenComponent<CounterComponent>(1);
+			builder.AddAttribute(2, "Class", "test");
+			builder.AddAttribute(3, "Data", 3);
+			builder.CloseComponent();
+		});
+
+		var stub = cut.FindComponent<CounterComponentStub>();
+		var captured = stub.Instance.UnmatchedValues;
+		Assert.Equal(2, captured.Count);
+		Assert.Equal("test", captured.GetValueOrDefault("Class", null));
+		Assert.Equal(3, captured.GetValueOrDefault("Data", 0));
 	}
 }
