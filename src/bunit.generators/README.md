57aa45184d7fb39588f59616c5332ca112855069
Steven Giesel <stgiesel35@gmail.com>
2023-11-29 21:24:08 +0100

    feat: Add generator for StubAttribute

--- src/bunit.generators/README.md
+++ src/bunit.generators/README.md
@@ -1,10 +1,11 @@
 # bUnit Generators
 
-This package contains source generators for bUnit, to make it easier and more convenient to write tests. 
+This package contains source generators for bUnit, to make it easier and more convenient to write tests.
 
 ## `AddStub` Generator
-This generator adds the ability to automatically generate stubs for a given type. This comes in handy, when dealing
-with 3rd party components that might need an extensive setup. Here a small example:
+This generator adds the ability to automatically generate stubs for a given type with no setup involved. The generator sits on top of the already
+present `AddStub` method.
+This comes in handy, when dealing with 3rd party components that might need an extensive setup. Here a small example:
 
 Given the following component
 ```razor
@@ -49,6 +50,31 @@ To use the generator, the **Interceptor** feature has to be used inside the cspr
 		<InterceptorsPreviewNamespaces>$(InterceptorsPreviewNamespaces);Bunit</InterceptorsPreviewNamespaces>
 ```
 
+This limits the usage to .NET 8 and above.
+
+## `StubAttribute`
+This generator adds the ability to automatically generate stubs for a given type via an attribute.
+The general setup for the given component above looks like this:
+```csharp
+namespace MyTest;
+
+public class FeatureTests : TestContext
+{
+    [Fact]
+    public void Test()
+    {
+        ComponentFactories.Add<ThirdPartyText, ThirdPartyStub>();
+        ...
+    }    
+}
+
+[Stub(typeof(ThirdPartyText))]
+internal partial class ThidPartyStub { }
+```
+
+Current limitations of this approach:
+ * The stubbed type is not allowed to be nested inside the test class.
+
 ## Developer notes
 
 ### Tips for developing with the generator
@@ -59,7 +85,7 @@ When changing the source generator, to see the effect, clearing the build cache 
 dotnet build-server shutdown
 ```
 
-A good way to quicky see if the generate is producing output:
+A good way to quickly see if the generate is producing output:
 
 ```
 dotnet build-server shutdown && dotnet clean && dotnet test -p:TargetFramework=net8.0
---
+++ src/bunit.generators/Web.Stubs/AttributeStubGenerator/StubAttributeGenerator.cs
@@ -0,0 +1,120 @@
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Bunit.Web.Stubs.AttributeStubGenerator;
+
+/// <summary>
+/// Generator that creates a stub of a marked component.
+/// </summary>
+[Generator]
+public class StubAttributeGenerator : IIncrementalGenerator
+{
+	private const string AttributeFullQualifiedName = "Bunit.StubAttribute";
+
+	public void Initialize(IncrementalGeneratorInitializationContext context)
+	{
+		context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+			"StubAttribute.g.cs",
+			SourceText.From(StubAttribute.StubAttributeSource, Encoding.UTF8)));
+
+		var classesToStub = context.SyntaxProvider
+			.ForAttributeWithMetadataName(
+				AttributeFullQualifiedName,
+				predicate: static (s, _) => s is ClassDeclarationSyntax,
+				transform: static (ctx, _) => GetStubClassInfo(ctx))
+			.Where(static m => m is not null);
+
+
+		context.RegisterSourceOutput(
+			classesToStub,
+			static (spc, source) => Execute(source, spc));
+	}
+
+	private static StubClassInfo GetStubClassInfo(GeneratorAttributeSyntaxContext context)
+	{
+		foreach (var attribute in context.TargetSymbol.GetAttributes())
+		{
+			if (context.TargetSymbol is not ITypeSymbol stubbedType)
+			{
+				continue;
+			}
+
+			var namespaceName = stubbedType.ContainingNamespace.ToDisplayString();
+			var className = context.TargetSymbol.Name;
+			var visibility = context.TargetSymbol.DeclaredAccessibility.ToString().ToLower();
+
+			var originalTypeToStub = attribute.ConstructorArguments.FirstOrDefault().Value;
+			if (originalTypeToStub is not ITypeSymbol originalType)
+			{
+				continue;
+			}
+
+			return new StubClassInfo
+			{
+				ClassName = className,
+				Namespace = namespaceName,
+				TargetType = originalType,
+				Visibility = visibility
+			};
+		}
+
+		return null;
+	}
+
+	private static void Execute(StubClassInfo classInfo, SourceProductionContext context)
+	{
+		var hasSomethingToStub = false;
+		var targetTypeSymbol = (INamedTypeSymbol)classInfo!.TargetType;
+		var sourceBuilder = new StringBuilder();
+
+		sourceBuilder.AppendLine($"namespace {classInfo.Namespace};");
+
+		sourceBuilder.AppendLine(
+			$"{classInfo.Visibility} partial class {classInfo.ClassName} : global::Microsoft.AspNetCore.Components.ComponentBase");
+		sourceBuilder.Append("{");
+
+		foreach (var member in targetTypeSymbol
+			         .GetMembers()
+			         .OfType<IPropertySymbol>()
+			         .Where(p => p.GetAttributes()
+				         .Any(attr =>
+					         attr.AttributeClass?.ToDisplayString() ==
+					         "Microsoft.AspNetCore.Components.ParameterAttribute" ||
+					         attr.AttributeClass?.ToDisplayString() ==
+					         "Microsoft.AspNetCore.Components.CascadingParameterAttribute")))
+		{
+			sourceBuilder.AppendLine();
+
+			hasSomethingToStub = true;
+			var propertyType = member.Type.ToDisplayString();
+			var propertyName = member.Name;
+
+			var isParameterAttribute = member.GetAttributes().Any(attr =>
+				attr.AttributeClass?.ToDisplayString() == "Microsoft.AspNetCore.Components.ParameterAttribute");
+			var attributeLine = isParameterAttribute
+				? "\t[global::Microsoft.AspNetCore.Components.Parameter]"
+				: "\t[global::Microsoft.AspNetCore.Components.CascadingParameter]";
+
+			sourceBuilder.AppendLine(attributeLine);
+			sourceBuilder.AppendLine($"\tpublic {propertyType} {propertyName} {{ get; set; }}");
+		}
+
+		sourceBuilder.AppendLine("}");
+
+		if (hasSomethingToStub)
+		{
+			context.AddSource($"{classInfo.ClassName}Stub.g.cs", sourceBuilder.ToString());
+		}
+	}
+}
+
+internal sealed class StubClassInfo
+{
+	public string ClassName { get; set; }
+	public string Namespace { get; set; }
+	public ITypeSymbol TargetType { get; set; }
+	public string Visibility { get; set; }
+}
