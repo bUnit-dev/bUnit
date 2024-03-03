67d51f31d15c44146c9d2972d18e79f333a3e098
Steven Giesel <stgiesel35@gmail.com>
2024-03-03 11:41:02 +0100

    refactor: Renamed TestRenderer to BunitRenderer

--- src/bunit/Rendering/RenderEvent.cs
+++ src/bunit/Rendering/RenderEvent.cs
@@ -1,7 +1,7 @@
 namespace Bunit.Rendering;
 
 /// <summary>
-/// Represents an render event from a <see cref="TestRenderer"/>.
+/// Represents an render event from a <see cref="BunitRenderer"/>.
 /// </summary>
 public sealed class RenderEvent
 {
