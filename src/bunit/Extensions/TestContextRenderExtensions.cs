5851b8c28702061ed5c0e48c9dad2462354b3f38
Steven Giesel <stgiesel35@gmail.com>
2024-03-03 11:11:12 +0100

    refactor: Merge TestContext with TestContextBase

--- src/bunit/Extensions/TestContextRenderExtensions.cs
+++ src/bunit/Extensions/TestContextRenderExtensions.cs
@@ -3,18 +3,18 @@ using Bunit.Rendering;
 namespace Bunit.Extensions;
 
 /// <summary>
-/// Extensions methods for <see cref="TestContextBase"/> types.
+/// Extensions methods for <see cref="TestContext"/> types.
 /// </summary>
-public static class TestContextBaseRenderExtensions
+public static class TestContextRenderExtensions
 {
 	/// <summary>
-	/// Renders a component, declared in the <paramref name="renderFragment"/>, inside the <see cref="TestContextBase.RenderTree"/>.
+	/// Renders a component, declared in the <paramref name="renderFragment"/>, inside the <see cref="TestContext.RenderTree"/>.
 	/// </summary>
 	/// <typeparam name="TComponent">The type of component to render.</typeparam>
 	/// <param name="testContext">Test context to use to render with.</param>
 	/// <param name="renderFragment">The <see cref="RenderInsideRenderTree"/> that contains a declaration of the component.</param>
 	/// <returns>A <see cref="IRenderedComponent{TComponent}"/>.</returns>
-	public static IRenderedComponent<TComponent> RenderInsideRenderTree<TComponent>(this TestContextBase testContext, RenderFragment renderFragment)
+	public static IRenderedComponent<TComponent> RenderInsideRenderTree<TComponent>(this TestContext testContext, RenderFragment renderFragment)
 		where TComponent : IComponent
 	{
 		ArgumentNullException.ThrowIfNull(testContext);
@@ -24,12 +24,12 @@ public static class TestContextBaseRenderExtensions
 	}
 
 	/// <summary>
-	/// Renders a fragment, declared in the <paramref name="renderFragment"/>, inside the <see cref="TestContextBase.RenderTree"/>.
+	/// Renders a fragment, declared in the <paramref name="renderFragment"/>, inside the <see cref="TestContext.RenderTree"/>.
 	/// </summary>
 	/// <param name="testContext">Test context to use to render with.</param>
 	/// <param name="renderFragment">The <see cref="RenderInsideRenderTree"/> to render.</param>
 	/// <returns>A <see cref="IRenderedFragment"/>.</returns>
-	public static IRenderedFragment RenderInsideRenderTree(this TestContextBase testContext, RenderFragment renderFragment)
+	public static IRenderedFragment RenderInsideRenderTree(this TestContext testContext, RenderFragment renderFragment)
 	{
 		ArgumentNullException.ThrowIfNull(testContext);
 
--- tests/bunit.tests/TestContextTest.cs
+++ tests/bunit.tests/TestContextTest.cs
@@ -1,9 +1,55 @@
+using Bunit.TestAssets.SampleComponents.DisposeComponents;
 using Bunit.TestDoubles;
 
 namespace Bunit;
 
 public partial class TestContextTest : TestContext
 {
+	[Fact(DisplayName = "DisposeComponents disposes rendered components in parent to child order")]
+	public void Test101()
+	{
+		var callStack = new List<string>();
+		RenderComponent<ParentDispose>(ps => ps.Add(p => p.CallStack, callStack));
+
+		DisposeComponents();
+
+		callStack.Count.ShouldBe(2);
+		callStack[0].ShouldBe("ParentDispose");
+		callStack[1].ShouldBe("ChildDispose");
+	}
+
+	[Fact(DisplayName = "DisposeComponents disposes multiple rendered components")]
+	public void Test102()
+	{
+		var callStack = new List<string>();
+		RenderComponent<ChildDispose>(ps => ps.Add(p => p.CallStack, callStack));
+		RenderComponent<ChildDispose>(ps => ps.Add(p => p.CallStack, callStack));
+
+		DisposeComponents();
+
+		callStack.Count.ShouldBe(2);
+	}
+
+	[Fact(DisplayName = "DisposeComponents rethrows exceptions from Dispose methods in components")]
+	public void Test103()
+	{
+		RenderComponent<ThrowExceptionComponent>();
+		var action = () => DisposeComponents();
+
+		action.ShouldThrow<NotSupportedException>();
+	}
+
+	[Fact(DisplayName = "DisposeComponents disposes components nested in render fragments")]
+	public void Test104()
+	{
+		var callStack = new List<string>();
+		Render(DisposeFragments.ChildDisposeAsFragment(callStack));
+
+		DisposeComponents();
+
+		callStack.Count.ShouldBe(1);
+	}
+	
 	[Fact(DisplayName = "The test service provider should register a placeholder HttpClient which throws exceptions")]
 	public void Test024()
 	{
@@ -74,11 +120,195 @@ public partial class TestContextTest : TestContext
 	{
 		Should.NotThrow(() => RenderComponent<ClickCounter>().Find("button").Click());
 	}
+	
+	[Fact(DisplayName = "TestContext should provide a default IErrorBoundaryLogger")]
+	public void Test001()
+	{
+		IErrorBoundaryLogger logger = Services.GetService<IErrorBoundaryLogger>();
+		logger.ShouldNotBe(null);
+	}
+	
+	[Fact(DisplayName = "ComponentFactories CanCreate() method are checked during component instantiation")]
+	public void Test0001()
+	{
+		var mock = CreateMockComponentFactory(canCreate: _ => false, create: _ => null);
+		ComponentFactories.Add(mock);
+
+		RenderComponent<Simple1>();
+
+		mock.Received(1).CanCreate(typeof(Simple1));
+		mock.DidNotReceive().Create(Arg.Any<Type>());
+	}
+
+	[Fact(DisplayName = "ComponentFactories Create() method is called when their CanCreate() method returns true")]
+	public void Test0002()
+	{
+		var mock = CreateMockComponentFactory(canCreate: _ => true, create: _ => new Simple1());
+		ComponentFactories.Add(mock);
+
+		RenderComponent<Simple1>();
+
+		mock.Received(1).CanCreate(typeof(Simple1));
+		mock.Received(1).Create(typeof(Simple1));
+	}
+
+	[Fact(DisplayName = "ComponentFactories is used in last added order")]
+	public void Test0003()
+	{
+		var firstMock = CreateMockComponentFactory(canCreate: _ => true, create: _ => new Simple1());
+		var secondMock = CreateMockComponentFactory(canCreate: _ => true, create: _ => new Simple1());
+		ComponentFactories.Add(firstMock);
+		ComponentFactories.Add(secondMock);
+
+		RenderComponent<Simple1>();
+
+		firstMock.DidNotReceive().CanCreate(Arg.Any<Type>());
+		firstMock.DidNotReceive().Create(Arg.Any<Type>());
+		secondMock.Received(1).CanCreate(typeof(Simple1));
+		secondMock.Received(1).Create(typeof(Simple1));
+	}
+
+	[Fact(DisplayName = "DisposeComponents captures exceptions from DisposeAsync in Renderer.UnhandledException")]
+	public async Task Test201()
+	{
+		var tcs = new TaskCompletionSource();
+		var expected = new NotSupportedException();
+		RenderComponent<AsyncThrowExceptionComponent>(
+			ps => ps.Add(p => p.DisposedTask, tcs.Task));
+
+		DisposeComponents();
+
+		tcs.SetException(expected);
+		var actual = await Renderer.UnhandledException;
+		actual.ShouldBeSameAs(expected);
+	}
+
+	[Fact(DisplayName = "DisposeComponents calls DisposeAsync on rendered components")]
+	public async Task Test202()
+	{
+		var cut = RenderComponent<AsyncDisposableComponent>();
+		var wasDisposedTask = cut.Instance.DisposedTask;
+
+		DisposeComponents();
+
+		await wasDisposedTask.ShouldCompleteWithin(TimeSpan.FromSeconds(1));
+	}
+
+	[Fact(DisplayName = "DisposeComponents should dispose components added via ComponentFactory")]
+	public void Test203()
+	{
+		ComponentFactories.Add<ChildDispose, MyChildDisposeStub>();
+		var cut = RenderComponent<ParentDispose>(ps => ps.Add(p => p.CallStack, new List<string>()));
+		var instance = cut.FindComponent<MyChildDisposeStub>().Instance;
+
+		DisposeComponents();
+
+		instance.WasDisposed.ShouldBeTrue();
+	}
+
+	[Fact(DisplayName = "Can correctly resolve and dispose of scoped disposable service")]
+	public void Net5Test001()
+	{
+		AsyncDisposableService asyncDisposable;
+		using (var sut = new TestContext())
+		{
+			sut.Services.AddScoped<AsyncDisposableService>();
+			asyncDisposable = sut.Services.GetService<AsyncDisposableService>();
+		}
+		asyncDisposable.IsDisposed.ShouldBeTrue();
+	}
+
+	[Fact(DisplayName = "Can correctly resolve and dispose of transient disposable service")]
+	public void Net5Test002()
+	{
+		AsyncDisposableService asyncDisposable;
+		using (var sut = new TestContext())
+		{
+			sut.Services.AddTransient<AsyncDisposableService>();
+			asyncDisposable = sut.Services.GetService<AsyncDisposableService>();
+		}
+		asyncDisposable.IsDisposed.ShouldBeTrue();
+	}
+
+	[Fact(DisplayName = "Can correctly resolve and dispose of singleton disposable service")]
+	public void Net5Test003()
+	{
+		AsyncDisposableService asyncDisposable;
+		using (var sut = new TestContext())
+		{
+			sut.Services.AddSingleton<AsyncDisposableService>();
+			asyncDisposable = sut.Services.GetService<AsyncDisposableService>();
+		}
+		asyncDisposable.IsDisposed.ShouldBeTrue();
+	}
 
 	private sealed class ReceivesCascadingValue : ComponentBase
 	{
 		[CascadingParameter] public string? Value { get; set; }
 
 		[Parameter] public object? Dummy { get; set; }
+	}
+	
+	private sealed class ThrowExceptionComponent : ComponentBase, IDisposable
+	{
+		public void Dispose()
+		{
+#pragma warning disable S3877
+			throw new NotSupportedException();
+#pragma warning restore S3877
+		}
+	}
+	
+	private sealed class AsyncDisposableService : IAsyncDisposable
+	{
+		public bool IsDisposed { get; private set; }
+
+		public ValueTask DisposeAsync()
+		{
+			IsDisposed = true;
+			return ValueTask.CompletedTask;
+		}
+	}
+
+	private sealed class MyChildDisposeStub : ComponentBase, IDisposable
+	{
+		public bool WasDisposed { get; private set; }
+
+		public void Dispose()
+		{
+			WasDisposed = true;
+		}
+	}
+
+	private sealed class AsyncThrowExceptionComponent : ComponentBase, IAsyncDisposable
+	{
+		[Parameter]
+		public Task DisposedTask { get; set; }
+
+		public async ValueTask DisposeAsync()
+		{
+			await DisposedTask;
+		}
+	}
+
+	private sealed class AsyncDisposableComponent : ComponentBase, IAsyncDisposable
+	{
+		private readonly TaskCompletionSource tsc = new();
+
+		public Task DisposedTask => tsc.Task;
+
+		public async ValueTask DisposeAsync()
+		{
+			await Task.Delay(10);
+			tsc.SetResult();
+		}
+	}
+
+	private static IComponentFactory CreateMockComponentFactory(Func<Type, bool> canCreate, Func<Type, IComponent> create)
+	{
+		var result = Substitute.For<IComponentFactory>();
+		result.CanCreate(Arg.Any<Type>()).Returns(call => canCreate((Type)call[0]));
+		result.Create(Arg.Any<Type>()).Returns(call => create((Type)call[0]));
+		return result;
 	}
 }
