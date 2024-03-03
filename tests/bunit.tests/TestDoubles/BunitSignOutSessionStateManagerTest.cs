b1c7844a94895ded8d4aedd267a01ab120a50d85
Steven Giesel <stgiesel35@gmail.com>
2024-03-03 17:32:14 +0100

    refactor: Remove Extensions methods for TestContext and moved them to Testcontext directly

--- tests/bunit.tests/TestDoubles/BunitSignOutSessionStateManagerTest.cs
+++ tests/bunit.tests/TestDoubles/BunitSignOutSessionStateManagerTest.cs
@@ -5,7 +5,7 @@ public class BunitSignOutSessionStateManagerTest : TestContext
 	[Theory, AutoData]
 	public void ShouldSignOut(string randomUserName)
 	{
-		this.AddAuthorization().SetAuthorized(randomUserName);
+		AddAuthorization().SetAuthorized(randomUserName);
 		var cut = Render<SignOutSessionManagerLoginDisplay>();
 
 		cut.Find("button").Click();
