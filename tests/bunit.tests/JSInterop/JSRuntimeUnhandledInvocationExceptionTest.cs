97eec0b0db3ed984085919420d6bd32430df912b
Steven Giesel <stgiesel35@gmail.com>
2024-03-04 20:53:48 +0100

    refactor: Remove net5 files and moved them to their main content

--- tests/bunit.tests/JSInterop/JSRuntimeUnhandledInvocationExceptionTest.cs
+++ tests/bunit.tests/JSInterop/JSRuntimeUnhandledInvocationExceptionTest.cs
@@ -292,6 +292,22 @@ public partial class JSRuntimeUnhandledInvocationExceptionTest
 
 		Assert.Equal(expectedErrorMessage, sut.Message);
 	}
+	
+	[Theory(DisplayName = "Message prints correctly when trying to import an unconfigured module")]
+	[AutoData]
+	public void Test036(string moduleName)
+	{
+		var identifier = "import";
+		var returnType = typeof(IJSObjectReference);
+		var invocationMethodName = "InvokeAsync";
+		var expectedErrorMessage = CreateExpectedErrorMessage(
+			$"{CodeIdent}{invocationMethodName}<{returnType.Name}>(\"{identifier}\", \"{moduleName}\")",
+			$"{CodeIdent}SetupModule(\"{moduleName}\")");
+
+		var sut = new JSRuntimeUnhandledInvocationException(new JSRuntimeInvocation(identifier, new object?[] { moduleName }, returnType, invocationMethodName));
+
+		Assert.Equal(expectedErrorMessage, sut.Message);
+	}
 
 	public class Dummy1 { }
 	public class Dummy2 { }
