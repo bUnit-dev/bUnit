f0d3769563789231afb75b257ad29b3193b61408
Steven Giesel <stgiesel35@gmail.com>
2024-03-06 21:38:32 +0100

    feat: Add IAsyncDiposable for TestContext

--- src/bunit/TestContext.cs
+++ src/bunit/TestContext.cs
@@ -7,7 +7,7 @@ namespace Bunit;
 /// <summary>
 /// A test context is a factory that makes it possible to create components under tests.
 /// </summary>
-public partial class TestContext : IDisposable
+public partial class TestContext : IDisposable, IAsyncDisposable
 {
 	private bool disposed;
 	private BunitRenderer? bunitRenderer;
@@ -70,17 +70,46 @@ public partial class TestContext : IDisposable
 		GC.SuppressFinalize(this);
 	}
 
+	/// <inheritdoc/>
+	public async ValueTask DisposeAsync()
+	{
+		await DisposeAsyncCore();
+
+		Dispose(disposing: false);
+		GC.SuppressFinalize(this);
+	}
+
+	/// <summary>
+	/// Disposes of the test context resources that are asynchronous, in particular it disposes the <see cref="Services"/>
+	/// service provider.s
+	/// </summary>
+	protected virtual async ValueTask DisposeAsyncCore()
+	{
+		if (disposed)
+			return;
+
+		disposed = true;
+
+		// Ensure the renderer is disposed before all others,
+		// otherwise a render cycle may be ongoing and try to access
+		// the service provider to perform operations.
+		if (bunitRenderer is not null)
+		{
+			await bunitRenderer.DisposeAsync();
+		}
+
+		await Services.DisposeAsync();
+	}
+
 	/// <summary>
 	/// Disposes of the test context resources, in particular it disposes the <see cref="Services"/>
-	/// service provider. Any async services registered with the service provider will disposed first,
-	/// but their disposal will not be awaited..
+	/// service provider.
 	/// </summary>
 	/// <remarks>
 	/// The disposing parameter should be false when called from a finalizer, and true when called from the
 	/// <see cref="Dispose()"/> method. In other words, it is true when deterministically called and false when non-deterministically called.
 	/// </remarks>
 	/// <param name="disposing">Set to true if called from <see cref="Dispose()"/>, false if called from a finalizer.f.</param>
-	[SuppressMessage("Reliability", "CA2012:Use ValueTasks correctly", Justification = "Explicitly ignoring DisposeAsync to avoid breaking changes to API surface.")]
 	protected virtual void Dispose(bool disposing)
 	{
 		if (disposed || !disposing)
@@ -91,18 +120,7 @@ public partial class TestContext : IDisposable
 		// Ensure the renderer is disposed before all others,
 		// otherwise a render cycle may be ongoing and try to access
 		// the service provider to perform operations.
-		if (bunitRenderer is IDisposable renderer)
-		{
-			renderer.Dispose();
-		}
-
-		// Ignore the async task as GetAwaiter().GetResult() can cause deadlock
-		// and implementing IAsyncDisposable in TestContext will be a breaking change.
-		//
-		// NOTE: This has to be called before Services.Dispose().
-		// If there are IAsyncDisposable services registered, calling Dispose first
-		// causes the service provider to throw an exception.
-		_ = Services.DisposeAsync();
+		bunitRenderer?.Dispose();
 
 		// The service provider should dispose of any
 		// disposable object it has created, when it is disposed.
