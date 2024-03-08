78b6214d3c8b1940c3d78484f7c98a7691240d91
Steven Giesel <stgiesel35@gmail.com>
2024-03-08 16:20:16 +0100

    feat: Add Obsolete attribute for removed RenderComponent methods

---
+++ src/bunit/BunitContext.Obsoletes.cs
@@ -0,0 +1,24 @@
+namespace Bunit;
+
+public partial class BunitContext
+{
+	/// <summary>
+	/// Use <see cref="Render{TComponent}(System.Action{Bunit.ComponentParameterCollectionBuilder{TComponent}}?)"/> instead.
+	/// </summary>
+	[Obsolete($"Use {nameof(Render)} instead.", true, UrlFormat = "https://bunit.dev/docs/migration")]
+	public RenderedComponent<TComponent> RenderComponent<TComponent>()
+		where TComponent : IComponent
+	{
+		throw new NotSupportedException($"Use {nameof(Render)}<{typeof(TComponent).Name}> instead.");
+	}
+
+	/// <summary>
+	/// Use <see cref="Render{TComponent}(System.Action{Bunit.ComponentParameterCollectionBuilder{TComponent}}?)"/> instead.
+	/// </summary>
+	[Obsolete($"Use {nameof(Render)} instead.", true, UrlFormat = "https://bunit.dev/docs/migration")]
+	public RenderedComponent<TComponent> RenderComponent<TComponent>(Action<ComponentParameterCollectionBuilder<TComponent>>? parameterBuilder)
+		where TComponent : IComponent
+	{
+		throw new NotSupportedException($"Use {nameof(Render)}<{typeof(TComponent).Name}> instead.");
+	}
+}
