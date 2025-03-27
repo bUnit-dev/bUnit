4f5c8e7369312935ea9a1132fdd725aa22bb7a2a
Egil Hansen <egil@assimilated.dk>
2025-03-27 14:26:40 -0700

    test ShouldSupportSelected

--- tests/bunit.web.query.tests/Roles/RoleQueryExtensionsTest.cs
+++ tests/bunit.web.query.tests/Roles/RoleQueryExtensionsTest.cs
@@ -23,4 +23,33 @@ public class RoleQueryExtensionsTest : BunitContext
 		cut.FindByRole(AriaRole.Dialog).MarkupMatches("<div role=\"dialog\">I am a dialog</div>");
 		Should.Throw<InvalidOperationException>(() => cut.FindByRole(AriaRole.Menuitem));
 	}
+
+
+	[Fact]
+	public async Task ShouldSupportSelected()
+	{
+		var cut = Render<Wrapper>(ps => ps.AddChildContent(
+			@"
+			<select>
+			  <option>Hi</option>
+			  <option selected>Hello</option>
+			</select>
+			<div>
+			  <div role=""option"" aria-selected=""true"">Hi</div>
+			  <div role=""option"" aria-selected=""false"">Hello</div>
+			</div>
+			"));
+
+		// Test finding options that are selected
+		var selectedOptions = cut.FindAllByRole(AriaRole.Option, new ByRoleOptions { Selected = true }).ToArray();
+		selectedOptions.Length.Should().Be(2);
+		selectedOptions[0].MarkupMatches("<option selected>Hello</option>");
+		selectedOptions[1].MarkupMatches("<div role=\"option\" aria-selected=\"true\">Hi</div>");
+
+		// Test finding options that are not selected
+		var unselectedOptions = cut.FindAllByRole(AriaRole.Option, new ByRoleOptions { Selected = false }).ToArray();
+		unselectedOptions.Length.Should().Be(2);
+		unselectedOptions[0].MarkupMatches("<option>Hi</option>");
+		unselectedOptions[1].MarkupMatches("<div role=\"option\" aria-selected=\"false\">Hello</div>");
+	}
 }
