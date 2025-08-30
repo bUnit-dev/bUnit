6d8cd746c48762a8f34d35c4fb17e13cbf2fe947
Steven Giesel <stgiesel35@gmail.com>
2025-08-30 15:50:49 +0200

    fix: Migrations url in obsolete attr

--- src/bunit/BunitContext.Obsoletes.cs
+++ src/bunit/BunitContext.Obsoletes.cs
@@ -5,7 +5,7 @@ public partial class BunitContext
 	/// <summary>
 	/// Use <see cref="Render{TComponent}(System.Action{Bunit.ComponentParameterCollectionBuilder{TComponent}}?)"/> instead.
 	/// </summary>
-	[Obsolete($"Use {nameof(Render)} instead.", true, UrlFormat = "https://bunit.dev/docs/migration")]
+	[Obsolete($"Use {nameof(Render)} instead.", true, UrlFormat = "https://bunit.dev/docs/migrations")]
 	public IRenderedComponent<TComponent> RenderComponent<TComponent>()
 		where TComponent : IComponent
 	{
@@ -15,7 +15,7 @@ public partial class BunitContext
 	/// <summary>
 	/// Use <see cref="Render{TComponent}(System.Action{Bunit.ComponentParameterCollectionBuilder{TComponent}}?)"/> instead.
 	/// </summary>
-	[Obsolete($"Use {nameof(Render)} instead.", true, UrlFormat = "https://bunit.dev/docs/migration")]
+	[Obsolete($"Use {nameof(Render)} instead.", true, UrlFormat = "https://bunit.dev/docs/migrations")]
 	public IRenderedComponent<TComponent> RenderComponent<TComponent>(Action<ComponentParameterCollectionBuilder<TComponent>>? parameterBuilder)
 		where TComponent : IComponent
 	{
--- src/bunit/TestContext.cs
+++ src/bunit/TestContext.cs
@@ -1,7 +1,7 @@
 namespace Bunit;
 
 /// <inheritdoc />
-[Obsolete($"Use {nameof(BunitContext)} instead. BunitContext will be removed in a future release.", false, UrlFormat = "https://bunit.dev/docs/migration")]
+[Obsolete($"Use {nameof(BunitContext)} instead. BunitContext will be removed in a future release.", false, UrlFormat = "https://bunit.dev/docs/migrations")]
 public class TestContext : BunitContext
 {
 }
