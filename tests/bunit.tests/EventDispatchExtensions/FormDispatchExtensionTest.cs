f4368b9a453dcb941a8375019ed0f0d716a63359
Steven Giesel <stgiesel35@gmail.com>
2025-10-09 21:17:01 +0200

    refactor: Added tests for form click behavior

---
+++ tests/bunit.tests/EventDispatchExtensions/FormDispatchExtensionTest.cs
@@ -0,0 +1,34 @@
+namespace Bunit.EventDispatchExtensions;
+
+public class FormDispatchExtensionTest : BunitContext
+{
+	[Fact]
+	public void ClickingOnSubmitButtonTriggersOnsubmitOfForm()
+	{
+		var cut = Render<FormWithButton>(p => p.Add(s => s.HasSubmitType, true));
+		
+		cut.Find("#submitter").Click();
+
+		cut.Instance.SubmitWasCalled.ShouldBeTrue();
+	}
+	
+	[Fact]
+	public void ButtonWithoutTypeIsConsideredSubmitAndTriggersOnsubmitOfForm()
+	{
+		var cut = Render<FormWithButton>(p => p.Add(s => s.HasSubmitType, false));
+		
+		cut.Find("#submitter").Click();
+
+		cut.Instance.SubmitWasCalled.ShouldBeTrue();
+	}
+	
+	[Fact]
+	public void ButtonThatIsNotSubmitShouldNotTrigger()
+	{
+		var cut = Render<FormWithButton>(p => p.Add(s => s.HasSubmitType, false));
+			
+		cut.Find("#other").Click();
+
+		cut.Instance.SubmitWasCalled.ShouldBeFalse();
+	}
+}
