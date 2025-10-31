78f010b66514f2b721cdf5d9bde67a62ec0401c8
Egil Hansen <egil@assimilated.dk>
2025-10-31 15:23:56 +0000

    test: create clicking on submit button outside triggers onsubmit of form

--- tests/bunit.tests/EventDispatchExtensions/FormDispatchExtensionTest.cs
+++ tests/bunit.tests/EventDispatchExtensions/FormDispatchExtensionTest.cs
@@ -6,29 +6,39 @@ public class FormDispatchExtensionTest : BunitContext
 	public void ClickingOnSubmitButtonTriggersOnsubmitOfForm()
 	{
 		var cut = Render<FormWithButton>(p => p.Add(s => s.HasSubmitType, true));
-		
+
 		cut.Find("#submitter").Click();
 
 		cut.Instance.SubmitWasCalled.ShouldBeTrue();
 	}
-	
+
 	[Fact]
 	public void ButtonWithoutTypeIsConsideredSubmitAndTriggersOnsubmitOfForm()
 	{
 		var cut = Render<FormWithButton>(p => p.Add(s => s.HasSubmitType, false));
-		
+
 		cut.Find("#submitter").Click();
 
 		cut.Instance.SubmitWasCalled.ShouldBeTrue();
 	}
-	
+
 	[Fact]
 	public void ButtonThatIsNotSubmitShouldNotTrigger()
 	{
 		var cut = Render<FormWithButton>(p => p.Add(s => s.HasSubmitType, false));
-			
+
 		cut.Find("#other").Click();
 
 		cut.Instance.SubmitWasCalled.ShouldBeFalse();
+	}
+
+	[Fact]
+	public void ClickingOnSubmitButtonOutsideTriggersOnsubmitOfForm()
+	{
+		var cut = Render<FormWithButtonOutside>();
+
+		cut.Find("#submitter").Click();
+
+		cut.Instance.SubmitWasCalled.ShouldBeTrue();
 	}
 }
